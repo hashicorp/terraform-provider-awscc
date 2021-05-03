71e0b62a9df56662c2a3f97730c7e67287bc17ed
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-05-03 14:01:19 -0400

    Test 'ToSnakeCase'.

--- internal/service/cloudformation/schema-generator/codegen_test.go
+++ internal/service/cloudformation/schema-generator/codegen_test.go
@@ -2,27 +2,51 @@ package generator
 
 import (
 	"testing"
-
-	cfschema "github.com/hashicorp/aws-cloudformation-resource-schema-sdk-go"
 )
 
-func TestRootPropertySchema(t *testing.T) {
-	resourceSchema, err := cfschema.NewResourceJsonSchemaPath("testdata/aws_logs_log_group.cf-resource-schema.json")
-	if err != nil {
-		t.Fatalf("error loading test schema: %s", err)
+func TestToSnakeCase(t *testing.T) {
+	testCases := []struct {
+		TestName string
+		Input    string
+		Expected string
+	}{
+		{
+			TestName: "empty string",
+		},
+		{
+			TestName: "no caps",
+			Input:    "abc0123",
+			Expected: "abc0123",
+		},
+		{
+			TestName: "first char capitalized",
+			Input:    "Abc0123",
+			Expected: "abc0123",
+		},
+		{
+			TestName: "last char capitalized",
+			Input:    "abc0123Z",
+			Expected: "abc0123z",
+		},
+		{
+			TestName: "all caps",
+			Input:    "ABCZ",
+			Expected: "a_b_c_z",
+		},
+		{
+			TestName: "mixture",
+			Input:    "abcDeFGh",
+			Expected: "abc_de_f_gh",
+		},
 	}
 
-	resource, err := resourceSchema.Resource()
-	if err != nil {
-		t.Fatalf("error parsing test schema: %s", err)
-	}
+	for _, testCase := range testCases {
+		t.Run(testCase.TestName, func(t *testing.T) {
+			got := ToSnakeCase(testCase.Input)
 
-	if err := resource.Expand(); err != nil {
-		t.Fatalf("error expanding JSON Pointer references: %s", err)
-	}
-
-	for propertyName, property := range resource.Properties {
-		t.Logf("[DEBUG] Found schema property (%s):\n%s", propertyName, property)
-		t.Logf("[DEBUG] Generated schema for property (%s): %s", propertyName, RootPropertySchema(resource, propertyName))
+			if testCase.Expected != got {
+				t.Errorf("got %q, expected %q", got, testCase.Expected)
+			}
+		})
 	}
 }
