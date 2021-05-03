cef0d432554cb0c5f9576068c0565b28168dd704
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-05-03 15:00:06 -0400

    Generate all the resources (eventually).

---
+++ internal/provider/resources.go
@@ -0,0 +1,3 @@
+//go:generate go run generators/schema/main.go -resource aws_logs_log_group -cfschema ../service/cloudformation/schema-generator/testdata/aws_logs_log_group.cf-resource-schema.json
+
+package provider
