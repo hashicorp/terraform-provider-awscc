f2dda74959050eb0999d4bd4d85f998259637c40
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-05-04 12:25:11 -0400

    Allow destination package name to be overridden.

--- internal/provider/resources.go
+++ internal/provider/resources.go
@@ -1,3 +1,3 @@
-//go:generate go run generators/schema/main.go -resource aws_logs_log_group -cfschema ../service/cloudformation/schema-generator/testdata/aws_logs_log_group.cf-resource-schema.json
+//go:generate go run generators/schema/main.go -resource aws_logs_log_group -cfschema ../service/cloudformation/schema-generator/testdata/aws_logs_log_group.cf-resource-schema.json -- aws_logs_log_group_schema_gen.go
 
 package provider
