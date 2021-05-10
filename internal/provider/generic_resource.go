dfe0a592da1e8c49e80cc51c3f5285b5d32557a8
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-05-10 11:06:33 -0400

    Add 'GenericResource.ResourceInstance'.

--- internal/provider/generic_resource.go
+++ internal/provider/generic_resource.go
@@ -25,71 +25,54 @@ func (g *GenericResource) GetSchema() *schema.Resource {
 	}
 }
 
-func (g *GenericResource) ResourceInstance() *GenericResourceInstance {
-	return nil
+func (g *GenericResource) ResourceInstance(ctx context.Context, meta interface{}) *GenericResourceInstance {
+	return &GenericResourceInstance{
+		GenericResource: g,
+		Meta:            meta,
+	}
 }
 
 // Create is the generic Create handler for a generated resource.
 func (g *GenericResource) Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	instance := GenericResourceInstance{
-		GenericResource: g,
-		ResourceData:    d,
-	}
-
-	return instance.Create(ctx)
+	return g.ResourceInstance(ctx, meta).Create(ctx, d)
 }
 
 // Read is the generic Read handler for a generated resource.
 func (g *GenericResource) Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	instance := GenericResourceInstance{
-		GenericResource: g,
-		ResourceData:    d,
-	}
-
-	return instance.Read(ctx)
+	return g.ResourceInstance(ctx, meta).Read(ctx, d)
 }
 
 // Update is the generic Update handler for a generated resource.
 func (g *GenericResource) Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	instance := GenericResourceInstance{
-		GenericResource: g,
-		ResourceData:    d,
-	}
-
-	return instance.Update(ctx)
+	return g.ResourceInstance(ctx, meta).Update(ctx, d)
 }
 
 // Delete is the generic Delete handler for a generated resource.
 func (g *GenericResource) Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	instance := GenericResourceInstance{
-		GenericResource: g,
-		ResourceData:    d,
-	}
-
-	return instance.Delete(ctx)
+	return g.ResourceInstance(ctx, meta).Delete(ctx, d)
 }
 
 type GenericResourceInstance struct {
 	GenericResource *GenericResource
-	ResourceData    *schema.ResourceData
+	Meta            interface{}
 }
 
 // Create is the generic Create handler for a generated resource.
-func (g *GenericResourceInstance) Create(ctx context.Context) diag.Diagnostics {
+func (g *GenericResourceInstance) Create(ctx context.Context, d *schema.ResourceData) diag.Diagnostics {
 	return nil
 }
 
 // Read is the generic Read handler for a generated resource.
-func (g *GenericResourceInstance) Read(ctx context.Context) diag.Diagnostics {
+func (g *GenericResourceInstance) Read(ctx context.Context, d *schema.ResourceData) diag.Diagnostics {
 	return nil
 }
 
 // Update is the generic Update handler for a generated resource.
-func (g *GenericResourceInstance) Update(ctx context.Context) diag.Diagnostics {
+func (g *GenericResourceInstance) Update(ctx context.Context, d *schema.ResourceData) diag.Diagnostics {
 	return nil
 }
 
 // Delete is the generic Delete handler for a generated resource.
-func (g *GenericResourceInstance) Delete(ctx context.Context) diag.Diagnostics {
+func (g *GenericResourceInstance) Delete(ctx context.Context, d *schema.ResourceData) diag.Diagnostics {
 	return nil
 }
