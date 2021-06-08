b40efd654c2696cdabecc6dfa2b264a33a0cfb4b
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-06-08 11:37:28 -0400

    Tweak generic resource.

--- internal/provider/generic_resource.go
+++ internal/provider/generic_resource.go
@@ -10,17 +10,23 @@ import (
 
 // Implements tfsdk.ResourceType.
 type genericResourceType struct {
-	schema schema.Schema
+	cfTypeName string        // CloudFormation type name for the resource type
+	tfSchema   schema.Schema // Terraform schema for the resource type
+	tfTypeName string        // Terraform type name for resource type
 }
 
-func NewGenericResourceType(schema schema.Schema) tfsdk.ResourceType {
+// NewGenericResourceType returns a new ResourceType representing the specified CloudFormation type.
+// It's public as it's called from generated code.
+func NewGenericResourceType(cfTypeName, tfTypeName string, tfSchema schema.Schema) tfsdk.ResourceType {
 	return &genericResourceType{
-		schema: schema,
+		cfTypeName: cfTypeName,
+		tfSchema:   tfSchema,
+		tfTypeName: tfTypeName,
 	}
 }
 
 func (rt *genericResourceType) GetSchema(_ context.Context) (schema.Schema, []*tfprotov6.Diagnostic) {
-	return rt.schema, nil
+	return rt.tfSchema, nil
 }
 
 func (rt *genericResourceType) NewResource(provider tfsdk.Provider) (tfsdk.Resource, []*tfprotov6.Diagnostic) {
@@ -29,14 +35,14 @@ func (rt *genericResourceType) NewResource(provider tfsdk.Provider) (tfsdk.Resou
 
 // Implements tfsdk.Resource.
 type genericResource struct {
-	client *awsClient
-	schema schema.Schema
+	client       *awsClient
+	resourceType *genericResourceType
 }
 
 func newGenericResource(provider *awsProvider, resourceType *genericResourceType) tfsdk.Resource {
 	return &genericResource{
-		client: provider.Client,
-		schema: resourceType.schema,
+		client:       provider.Client,
+		resourceType: resourceType,
 	}
 }
 
