beaf66dc66fa2f931f6398874c102ea0e4c666ae
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-06-25 10:01:04 -0400

    Use new Plugin bootstrap.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -12,22 +12,21 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	tflog "github.com/hashicorp/terraform-plugin-log"
 	"github.com/hashicorp/terraform-provider-aws-cloudapi/internal/registry"
 )
 
-func New(version string) tfsdk.Provider {
-	return &awsProvider{}
+func New() tfsdk.Provider {
+	return &awsCloudAPIProvider{}
 }
 
-type awsClient struct {
-	cfconn *cloudformation.CloudFormation
+type awsCloudAPIProvider struct {
+	cfClient *cloudformation.CloudFormation
 }
 
-type awsProvider struct {
-	client *awsClient
-}
+func (p *awsCloudAPIProvider) GetSchema(ctx context.Context) (schema.Schema, []*tfprotov6.Diagnostic) {
+	tflog.Debug(ctx, "Provider.GetSchema() enter")
 
-func (p *awsProvider) GetSchema(_ context.Context) (schema.Schema, []*tfprotov6.Diagnostic) {
 	return schema.Schema{
 		Version: 1,
 		Attributes: map[string]schema.Attribute{
@@ -46,17 +45,52 @@ func (p *awsProvider) GetSchema(_ context.Context) (schema.Schema, []*tfprotov6.
 	}, nil
 }
 
-func (p *awsProvider) Configure(_ context.Context, input tfsdk.ConfigureProviderRequest, output *tfsdk.ConfigureProviderResponse) {
-	client, err := newAWSClient()
-
-	if err != nil {
-		//return nil, appendDiagnostic(nil, fmt.Errorf("error configuring Terraform AWS Provider: %w", err))
-	}
-
-	p.client = client
+type providerData struct {
+	Region  types.String `tfsdk:"region"`
+	RoleARN types.String `tfsdk:"role_arn"`
 }
 
-func (p *awsProvider) GetResources(ctx context.Context) (map[string]tfsdk.ResourceType, []*tfprotov6.Diagnostic) {
+func (p *awsCloudAPIProvider) Configure(ctx context.Context, input tfsdk.ConfigureProviderRequest, output *tfsdk.ConfigureProviderResponse) {
+	tflog.Debug(ctx, "Provider.Configure() enter")
+
+	var config providerData
+
+	err := input.Config.Get(ctx, &config)
+
+	if err != nil {
+		output.Diagnostics = append(output.Diagnostics, &tfprotov6.Diagnostic{
+			Severity: tfprotov6.DiagnosticSeverityError,
+			Summary:  "Error parsing provider configuration",
+			Detail:   fmt.Sprintf("Error parsing the provider configuration, this is an error in the provider.\n%s\n", err),
+		})
+
+		return
+	}
+
+	if config.Region.Null || config.Region.Unknown || config.RoleARN.Null || config.RoleARN.Unknown {
+		tflog.Info(ctx, "One or more configuration values is Null or Unknown")
+
+		return
+	}
+
+	cfClient, err := newCloudFormationClient(&config)
+
+	if err != nil {
+		output.Diagnostics = append(output.Diagnostics, &tfprotov6.Diagnostic{
+			Severity: tfprotov6.DiagnosticSeverityError,
+			Summary:  "Error configuring AWS CloudFormation client",
+			Detail:   fmt.Sprintf("Error configuring the AWS CloudFormation client, this is an error in the provider.\n%s\n", err),
+		})
+
+		return
+	}
+
+	p.cfClient = cfClient
+}
+
+func (p *awsCloudAPIProvider) GetResources(ctx context.Context) (map[string]tfsdk.ResourceType, []*tfprotov6.Diagnostic) {
+	tflog.Debug(ctx, "Provider.GetResources() enter")
+
 	var diags []*tfprotov6.Diagnostic
 	resources := make(map[string]tfsdk.ResourceType)
 
@@ -64,7 +98,11 @@ func (p *awsProvider) GetResources(ctx context.Context) (map[string]tfsdk.Resour
 		resourceType, err := factory(ctx)
 
 		if err != nil {
-			diags = appendDiagnostic(diags, err)
+			diags = append(diags, &tfprotov6.Diagnostic{
+				Severity: tfprotov6.DiagnosticSeverityError,
+				Summary:  "Error getting Resource",
+				Detail:   fmt.Sprintf("Error getting the %s Resource, this is an error in the provider.\n%s\n", name, err),
+			})
 
 			continue
 		}
@@ -75,63 +113,21 @@ func (p *awsProvider) GetResources(ctx context.Context) (map[string]tfsdk.Resour
 	return resources, diags
 }
 
-func (p *awsProvider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, []*tfprotov6.Diagnostic) {
+func (p *awsCloudAPIProvider) GetDataSources(ctx context.Context) (map[string]tfsdk.DataSourceType, []*tfprotov6.Diagnostic) {
+	tflog.Debug(ctx, "Provider.GetDataSources() enter")
+
 	return nil, nil
 }
 
-func (p *awsProvider) CloudFormationClient(_ context.Context) (*cloudformation.CloudFormation, error) {
-	return p.client.cfconn, nil
+func (p *awsCloudAPIProvider) CloudFormationClient(_ context.Context) (*cloudformation.CloudFormation, error) {
+	return p.cfClient, nil
 }
 
-/*
-func New(version string) func() *schema.Provider {
-	return func() *schema.Provider {
-		p := &schema.Provider{
-			Schema: map[string]*schema.Schema{
-				"region": {
-					Type:     schema.TypeString,
-					Required: true,
-					DefaultFunc: schema.MultiEnvDefaultFunc([]string{
-						"AWS_REGION",
-						"AWS_DEFAULT_REGION",
-					}, nil),
-					Description:  "The region where AWS operations will take place.",
-					InputDefault: "us-east-1",
-				},
-
-				"role_arn": {
-					Type:        schema.TypeString,
-					Optional:    true,
-					Description: "Amazon Resource Name of an IAM Role that is used to do the actual provisioning.",
-				},
-			},
-
-			ResourcesMap: resources,
-		}
-
-		p.ConfigureContextFunc = configure(version, p)
-
-		return p
-	}
-}
-*/
-/*
-func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		config := Config{
-			Region: d.Get("region").(string),
-		}
-
-		return config.Client()
-	}
-}
-*/
-
-// newAWSClient configures and returns a fully initialized AWS client.
-func newAWSClient() (*awsClient, error) {
+// newCloudFormationClient configures and returns a fully initialized AWS CloudFormation client.
+func newCloudFormationClient(config *providerData) (*cloudformation.CloudFormation, error) {
 	awsbaseConfig := &awsbase.Config{
 		//DebugLogging: logging.IsDebugOrHigher(),
-		Region: "us-west-2",
+		Region: config.Region.Value,
 	}
 
 	sess, _, _, err := awsbase.GetSessionWithAccountIDAndPartition(awsbaseConfig)
@@ -139,27 +135,5 @@ func newAWSClient() (*awsClient, error) {
 		return nil, fmt.Errorf("error getting AWS SDK session: %w", err)
 	}
 
-	client := &awsClient{
-		cfconn: cloudformation.New(sess.Copy(&aws.Config{})),
-	}
-
-	return client, nil
-}
-
-// appendDiagnostic appends an error or warning message to a response's diagnostics.
-func appendDiagnostic(diags []*tfprotov6.Diagnostic, d interface{}) []*tfprotov6.Diagnostic {
-	switch d := d.(type) {
-	case error:
-		diags = append(diags, &tfprotov6.Diagnostic{
-			Severity: tfprotov6.DiagnosticSeverityError,
-			Summary:  d.Error(),
-		})
-	case string:
-		diags = append(diags, &tfprotov6.Diagnostic{
-			Severity: tfprotov6.DiagnosticSeverityError,
-			Summary:  d,
-		})
-	}
-
-	return diags
+	return cloudformation.New(sess.Copy(&aws.Config{})), nil
 }
--- main.go
+++ main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"flag"
+	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-provider-aws-cloudapi/internal/provider"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
-
-var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary
-	version string = "dev"
-
-	// goreleaser can also pass the specific commit if you want
-	// commit  string = ""
-)
-
 func main() {
-	var debugMode bool
-
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
-	provider.New(version)
+	tfsdk.Serve(context.Background(), provider.New, tfsdk.ServeOpts{
+		Name: "aws-cloudapi",
+	})
 }
