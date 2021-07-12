e89b9b60500b8809566d0c1ed7e77f98f108a349
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-07-12 10:56:32 -0400

    Merge 'plan.go' into 'state.go'.

--- internal/generic/state_test.go
+++ internal/generic/state_test.go
@@ -364,3 +364,155 @@ func TestSetUnknownValuesFromCloudFormationResourceModel(t *testing.T) {
 		})
 	}
 }
+
+func makeSimpleTestPlan() tfsdk.Plan {
+	return tfsdk.Plan{
+		Raw: tftypes.NewValue(tftypes.Object{
+			AttributeTypes: map[string]tftypes.Type{
+				"name": tftypes.String,
+			},
+		}, map[string]tftypes.Value{
+			"name": tftypes.NewValue(tftypes.String, "testing"),
+		}),
+		Schema: testComplexSchema,
+	}
+}
+
+func makeSimpleTestPlanWithOptionalPopulated() tfsdk.Plan {
+	return tfsdk.Plan{
+		Raw: tftypes.NewValue(tftypes.Object{
+			AttributeTypes: map[string]tftypes.Type{
+				"name":   tftypes.String,
+				"number": tftypes.Number,
+			},
+		}, map[string]tftypes.Value{
+			"name":   tftypes.NewValue(tftypes.String, "testing"),
+			"number": tftypes.NewValue(tftypes.Number, 42),
+		}),
+		Schema: testSimpleSchema,
+	}
+}
+
+func makeComplexTestPlan() tfsdk.Plan {
+	return tfsdk.Plan{
+		Raw: tftypes.NewValue(tftypes.Object{
+			AttributeTypes: map[string]tftypes.Type{
+				"name":         tftypes.String,
+				"machine_type": tftypes.String,
+				"tags":         tftypes.List{ElementType: tftypes.String},
+				"disks": tftypes.List{
+					ElementType: diskElementType,
+				},
+				"boot_disk": diskElementType,
+				"scratch_disk": tftypes.Object{
+					AttributeTypes: map[string]tftypes.Type{
+						"interface": tftypes.String,
+					},
+				},
+			},
+		}, map[string]tftypes.Value{
+			"name":         tftypes.NewValue(tftypes.String, "hello, world"),
+			"machine_type": tftypes.NewValue(tftypes.String, "e2-medium"),
+			"tags": tftypes.NewValue(tftypes.List{
+				ElementType: tftypes.String,
+			}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "red"),
+				tftypes.NewValue(tftypes.String, "blue"),
+				tftypes.NewValue(tftypes.String, "green"),
+			}),
+			"disks": tftypes.NewValue(tftypes.List{
+				ElementType: diskElementType,
+			}, []tftypes.Value{
+				tftypes.NewValue(diskElementType, map[string]tftypes.Value{
+					"id":                   tftypes.NewValue(tftypes.String, "disk0"),
+					"delete_with_instance": tftypes.NewValue(tftypes.Bool, true),
+				}),
+				tftypes.NewValue(diskElementType, map[string]tftypes.Value{
+					"id":                   tftypes.NewValue(tftypes.String, "disk1"),
+					"delete_with_instance": tftypes.NewValue(tftypes.Bool, false),
+				}),
+			}),
+			"boot_disk": tftypes.NewValue(diskElementType, map[string]tftypes.Value{
+				"id":                   tftypes.NewValue(tftypes.String, "bootdisk"),
+				"delete_with_instance": tftypes.NewValue(tftypes.Bool, true),
+			}),
+			"scratch_disk": tftypes.NewValue(tftypes.Object{
+				AttributeTypes: map[string]tftypes.Type{
+					"interface": tftypes.String,
+				},
+			}, map[string]tftypes.Value{
+				"interface": tftypes.NewValue(tftypes.String, "SCSI"),
+			}),
+		}),
+		Schema: testComplexSchema,
+	}
+}
+
+func TestPlanGetCloudFormationDesiredState(t *testing.T) {
+	testCases := []struct {
+		TestName      string
+		Plan          tfsdk.Plan
+		ExpectedError bool
+		ExpectedState map[string]interface{}
+	}{
+		{
+			TestName: "simple Plan",
+			Plan:     makeSimpleTestPlan(),
+			ExpectedState: map[string]interface{}{
+				"Name": "testing",
+			},
+		},
+		{
+			TestName: "simple Plan with Optional",
+			Plan:     makeSimpleTestPlanWithOptionalPopulated(),
+			ExpectedState: map[string]interface{}{
+				"Name":   "testing",
+				"Number": float64(42),
+			},
+		},
+		{
+			TestName: "complex Plan",
+			Plan:     makeComplexTestPlan(),
+			ExpectedState: map[string]interface{}{
+				"Name":        "hello, world",
+				"MachineType": "e2-medium",
+				"Tags":        []interface{}{"red", "blue", "green"},
+				"Disks": []interface{}{
+					map[string]interface{}{
+						"Id":                 "disk0",
+						"DeleteWithInstance": true,
+					},
+					map[string]interface{}{
+						"Id":                 "disk1",
+						"DeleteWithInstance": false,
+					},
+				},
+				"BootDisk": map[string]interface{}{
+					"Id":                 "bootdisk",
+					"DeleteWithInstance": true,
+				},
+				"ScratchDisk": map[string]interface{}{
+					"Interface": "SCSI",
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.TestName, func(t *testing.T) {
+			got, err := GetCloudFormationDesiredStateRaw(context.TODO(), testCase.Plan.Raw)
+
+			if err == nil && testCase.ExpectedError {
+				t.Fatalf("expected error")
+			}
+
+			if err != nil && !testCase.ExpectedError {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if diff := cmp.Diff(got, testCase.ExpectedState); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
+	}
+}
