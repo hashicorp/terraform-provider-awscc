f4e0839d8b2132911aed9806fbac02a1df8cc68a
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-07-16 15:06:32 -0400

    Name conversion functions.

---
+++ internal/strcase/strcase.go
@@ -0,0 +1,13 @@
+package strcase
+
+// CloudFormationPropertyNameToTerraformAttributeName converts a CloudFormation property name to a Terraform attribute name.
+// For example `GlobalReplicationGroupDescription` -> `global_replication_group_description`.
+func CloudFormationPropertyNameToTerraformAttributeName(propertyName string) string {
+	return ""
+}
+
+// TerraformAttributeNameToCloudFormationPropertyName converts a Terraform attribute name to a CloudFormation property name.
+// For example `global_replication_group_description` -> `GlobalReplicationGroupDescription`.
+func TerraformAttributeNameToCloudFormationPropertyName(propertyName string) string {
+	return ""
+}
---
+++ internal/strcase/strcase_test.go
@@ -0,0 +1,73 @@
+package strcase
+
+import (
+	"testing"
+)
+
+func TestCloudFormationPropertyNameToTerraformAttributeName(t *testing.T) {
+	testCases := []struct {
+		TestName      string
+		Value         string
+		ExpectedValue string
+	}{
+		{
+			TestName:      "short property name",
+			Value:         "Arn",
+			ExpectedValue: "arn",
+		},
+		{
+			TestName:      "long property name",
+			Value:         "GlobalReplicationGroupDescription",
+			ExpectedValue: "global_replication_group_description",
+		},
+		{
+			TestName:      "including digit",
+			Value:         "S3Bucket",
+			ExpectedValue: "s3_bucket",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.TestName, func(t *testing.T) {
+			got := CloudFormationPropertyNameToTerraformAttributeName(testCase.Value)
+
+			if got != testCase.ExpectedValue {
+				t.Errorf("expected: %s, got: %s", testCase.ExpectedValue, got)
+			}
+		})
+	}
+}
+
+func TestTerraformAttributeNameToCloudFormationPropertyName(t *testing.T) {
+	testCases := []struct {
+		TestName      string
+		Value         string
+		ExpectedValue string
+	}{
+		{
+			TestName:      "short property name",
+			Value:         "arn",
+			ExpectedValue: "Arn",
+		},
+		{
+			TestName:      "long property name",
+			Value:         "global_replication_group_description",
+			ExpectedValue: "GlobalReplicationGroupDescription",
+		},
+		{
+			TestName:      "including digit",
+			Value:         "s3_bucket",
+			ExpectedValue: "S3Bucket",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.TestName, func(t *testing.T) {
+			got := TerraformAttributeNameToCloudFormationPropertyName(testCase.Value)
+
+			if got != testCase.ExpectedValue {
+				t.Errorf("expected: %s, got: %s", testCase.ExpectedValue, got)
+			}
+		})
+	}
+}
