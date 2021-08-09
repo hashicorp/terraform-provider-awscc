a4a967fd468a8cebaebab7e88ecc2e2bb8ca72e8
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-08-09 10:00:22 -0400

    Add 'naming.ParseTerraformTypeName.

--- internal/naming/type_name_test.go
+++ internal/naming/type_name_test.go
@@ -36,6 +36,11 @@ func TestParseCloudFormationTypeName(t *testing.T) {
 			ExpectError: true,
 		},
 		{
+			TestName:    "Terraform type name",
+			Value:       "aws_kms_key",
+			ExpectError: true,
+		},
+		{
 			TestName:             "valid type name",
 			Value:                "AWS::KMS::Key",
 			ExpectedOrganization: "AWS",
@@ -47,6 +52,81 @@ func TestParseCloudFormationTypeName(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.TestName, func(t *testing.T) {
 			gotOrganization, gotService, gotResource, err := naming.ParseCloudFormationTypeName(testCase.Value)
+
+			if err == nil && testCase.ExpectError {
+				t.Fatalf("expected error, got no error")
+			}
+
+			if err != nil && !testCase.ExpectError {
+				t.Fatalf("got unexpected error: %s", err)
+			}
+
+			if gotOrganization != testCase.ExpectedOrganization {
+				t.Errorf("expected Organization: %s, got: %s", testCase.ExpectedOrganization, gotOrganization)
+			}
+			if gotService != testCase.ExpectedService {
+				t.Errorf("expected Service: %s, got: %s", testCase.ExpectedService, gotService)
+			}
+			if gotResource != testCase.ExpectedResource {
+				t.Errorf("expected Resource: %s, got: %s", testCase.ExpectedResource, gotResource)
+			}
+		})
+	}
+}
+
+func TestParseTerraformTypeName(t *testing.T) {
+	testCases := []struct {
+		TestName             string
+		Value                string
+		ExpectError          bool
+		ExpectedOrganization string
+		ExpectedService      string
+		ExpectedResource     string
+	}{
+		{
+			TestName:    "empty string",
+			Value:       "",
+			ExpectError: true,
+		},
+		{
+			TestName:    "whitespace string",
+			Value:       "  ",
+			ExpectError: true,
+		},
+		{
+			TestName:    "incorrect number of segments",
+			Value:       "aws_ec2",
+			ExpectError: true,
+		},
+		{
+			TestName:    "invalid type name",
+			Value:       "aws_kms_k-y",
+			ExpectError: true,
+		},
+		{
+			TestName:    "CloudFormation type name",
+			Value:       "AWS::KMS::Key",
+			ExpectError: true,
+		},
+		{
+			TestName:             "valid type name",
+			Value:                "aws_kms_key",
+			ExpectedOrganization: "aws",
+			ExpectedService:      "kms",
+			ExpectedResource:     "key",
+		},
+		{
+			TestName:             "valid type name multiple underscores in resource",
+			Value:                "aws_logs_log_group",
+			ExpectedOrganization: "aws",
+			ExpectedService:      "logs",
+			ExpectedResource:     "log_group",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.TestName, func(t *testing.T) {
+			gotOrganization, gotService, gotResource, err := naming.ParseTerraformTypeName(testCase.Value)
 
 			if err == nil && testCase.ExpectError {
 				t.Fatalf("expected error, got no error")
