56dcc9c7fd18c1ad8bb63da4479b33fee3fd2f63
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-08-18 16:59:18 -0400

    Correct package path.

--- internal/types/ordered_set_test.go
+++ internal/types/ordered_set_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
-	"github.com/hashicorp/terraform-provider-aws-cloudapi/internal/tfresource"
+	"github.com/hashicorp/terraform-provider-awscc/internal/tfresource"
 )
 
 func TestOrderedSetTypeValidate(t *testing.T) {
