4b7946a5d6336b4af02aa4a1c8eab534afc6444c
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-08-31 09:28:05 -0400

    Add 'TestResourceConfigRequiredAttributesValidator'.

--- internal/validate/required_test.go
+++ internal/validate/required_test.go
@@ -686,3 +686,122 @@ func TestRequiredAttributesValidator_Set(t *testing.T) {
 		})
 	}
 }
+
+func TestResourceConfigRequiredAttributesValidator(t *testing.T) {
+	t.Parallel()
+
+	objectElementType := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"alpha": tftypes.String,
+			"beta":  tftypes.String,
+			"gamma": tftypes.Bool,
+			"delta": tftypes.Number,
+		},
+	}
+
+	type testCase struct {
+		val         tftypes.Value
+		fs          []RequiredAttributesFunc
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"not an object": {
+			val:         tftypes.NewValue(tftypes.Bool, true),
+			expectError: true,
+		},
+		"nil object": {
+			val: tftypes.NewValue(objectElementType, nil),
+			fs:  []RequiredAttributesFunc{Required("alpha")},
+		},
+		"unknown object": {
+			val: tftypes.NewValue(objectElementType, tftypes.UnknownValue),
+			fs:  []RequiredAttributesFunc{Required("alpha")},
+		},
+		"not fully known object": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, nil),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, tftypes.UnknownValue),
+				"delta": tftypes.NewValue(tftypes.Number, nil),
+			}),
+			fs: []RequiredAttributesFunc{Required("alpha")},
+		},
+		"none required": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, "a"),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, true),
+				"delta": tftypes.NewValue(tftypes.Number, 42),
+			}),
+		},
+		"one required OK": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, "a"),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, true),
+				"delta": tftypes.NewValue(tftypes.Number, 42),
+			}),
+			fs: []RequiredAttributesFunc{Required("alpha")},
+		},
+		"one required error": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, "a"),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, true),
+				"delta": tftypes.NewValue(tftypes.Number, 42),
+			}),
+			fs:          []RequiredAttributesFunc{Required("beta")},
+			expectError: true,
+		},
+		"two required OK anyOf": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, "a"),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, true),
+				"delta": tftypes.NewValue(tftypes.Number, 42),
+			}),
+			fs: []RequiredAttributesFunc{AnyOfRequired(Required("beta"), AllOfRequired(Required("gamma"), Required("delta")))},
+		},
+		"two required OK oneOf": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, "a"),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, true),
+				"delta": tftypes.NewValue(tftypes.Number, 42),
+			}),
+			fs: []RequiredAttributesFunc{OneOfRequired(Required("beta"), AllOfRequired(Required("gamma"), Required("delta")))},
+		},
+		"two required error oneOf": {
+			val: tftypes.NewValue(objectElementType, map[string]tftypes.Value{
+				"alpha": tftypes.NewValue(tftypes.String, "a"),
+				"beta":  tftypes.NewValue(tftypes.String, nil),
+				"gamma": tftypes.NewValue(tftypes.Bool, true),
+				"delta": tftypes.NewValue(tftypes.Number, 42),
+			}),
+			fs:          []RequiredAttributesFunc{OneOfRequired(Required("alpha"), AllOfRequired(Required("gamma"), Required("delta")))},
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			request := tfsdk.ValidateResourceConfigRequest{
+				Config: tfsdk.Config{
+					Raw: test.val,
+				},
+			}
+			response := tfsdk.ValidateResourceConfigResponse{}
+			ResourceConfigRequiredAttributes(test.fs...).Validate(ctx, request, &response)
+
+			if !tfresource.DiagsHasError(response.Diagnostics) && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if tfresource.DiagsHasError(response.Diagnostics) && !test.expectError {
+				t.Fatalf("got unexpected error: %s", tfresource.DiagsError(response.Diagnostics))
+			}
+		})
+	}
+}
