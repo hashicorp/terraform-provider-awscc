dbb9b38d71fefc754c8923a203530fb8099e7f45
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-09-29 13:46:34 -0400

    'AttributePath.WithElementKeyInt' now requires int: https://github.com/hashicorp/terraform-plugin-go/pull/101.

--- internal/validate/for_each.go
+++ internal/validate/for_each.go
@@ -40,7 +40,7 @@ func (validator arrayForEachValidator) Validate(ctx context.Context, request tfs
 	}
 
 	for i, e := range l.Elems {
-		elemPath := request.AttributePath.WithElementKeyInt(int64(i))
+		elemPath := request.AttributePath.WithElementKeyInt(i)
 
 		elemRequest := tfsdk.ValidateAttributeRequest{
 			AttributePath:   elemPath,
