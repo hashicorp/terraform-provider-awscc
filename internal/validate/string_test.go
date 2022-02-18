b3ef3c22a44c60fb3b3509add237a2f0f0a87655
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-02-18 16:41:28 -0500

    Add 'validate.StringMatch'.

--- internal/validate/string_test.go
+++ internal/validate/string_test.go
@@ -2,6 +2,7 @@ package validate
 
 import (
 	"context"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
@@ -273,6 +274,70 @@ func TestStringInSliceValidator(t *testing.T) {
 			}
 			response := tfsdk.ValidateAttributeResponse{}
 			StringInSlice(test.valid).Validate(ctx, request, &response)
+
+			if !response.Diagnostics.HasError() && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if response.Diagnostics.HasError() && !test.expectError {
+				t.Fatalf("got unexpected error: %s", tfresource.DiagsError(response.Diagnostics))
+			}
+		})
+	}
+}
+
+func TestStringMatchValidator(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val         tftypes.Value
+		f           func(context.Context, tftypes.Value) (attr.Value, error)
+		re          *regexp.Regexp
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"not a string": {
+			val:         tftypes.NewValue(tftypes.Bool, true),
+			f:           types.BoolType.ValueFromTerraform,
+			expectError: true,
+		},
+		"unknown string": {
+			val: tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
+			f:   types.StringType.ValueFromTerraform,
+		},
+		"null string": {
+			val: tftypes.NewValue(tftypes.String, nil),
+			f:   types.StringType.ValueFromTerraform,
+		},
+		"valid string": {
+			val: tftypes.NewValue(tftypes.String, "ok"),
+			f:   types.StringType.ValueFromTerraform,
+			re:  regexp.MustCompile(`^o[j-l]?$`),
+		},
+		"invalid string": {
+			val:         tftypes.NewValue(tftypes.String, "not ok"),
+			f:           types.StringType.ValueFromTerraform,
+			re:          regexp.MustCompile(`\d+`),
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			val, err := test.f(ctx, test.val)
+
+			if err != nil {
+				t.Fatalf("got unexpected error: %s", err)
+			}
+
+			request := tfsdk.ValidateAttributeRequest{
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+				AttributeConfig: val,
+			}
+			response := tfsdk.ValidateAttributeResponse{}
+			StringMatch(test.re, "").Validate(ctx, request, &response)
 
 			if !response.Diagnostics.HasError() && test.expectError {
 				t.Fatal("expected error, got no error")
