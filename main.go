4777624ddaca500b314ab15cbaa96a14545ea46d
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-06-06 11:14:07 -0400

    Add '-debug' command-line flag.

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"flag"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-provider-awscc/internal/provider"
 )
 
 func main() {
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/hashicorp/awscc",
+		Debug:   debugMode,
 	})
 }
