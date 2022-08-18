fc66f6ba86e5a1739f7eb80dddcda66a80e297e2
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-08-18 09:27:07 -0400

    internal/registry: New Go packages: https://github.com/hashicorp/terraform-plugin-framework/pull/432.

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"sync"
 
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
 )
 
 var dataSourceRegisrationClosed bool
-var dataSourceRegistry map[string]func(context.Context) (tfsdk.DataSourceType, error)
+var dataSourceRegistry map[string]func(context.Context) (provider.DataSourceType, error)
 var dataSourceRegistryMu sync.Mutex
 
 var resourceRegisrationClosed bool
-var resourceRegistry map[string]func(context.Context) (tfsdk.ResourceType, error)
+var resourceRegistry map[string]func(context.Context) (provider.ResourceType, error)
 var resourceRegistryMu sync.Mutex
 
 // AddDataSourceTypeFactory registers the specified data source type name and factory.
-func AddDataSourceTypeFactory(name string, factory func(context.Context) (tfsdk.DataSourceType, error)) {
+func AddDataSourceTypeFactory(name string, factory func(context.Context) (provider.DataSourceType, error)) {
 	dataSourceRegistryMu.Lock()
 	defer dataSourceRegistryMu.Unlock()
 
@@ -25,13 +25,13 @@ func AddDataSourceTypeFactory(name string, factory func(context.Context) (tfsdk.
 	}
 
 	if dataSourceRegistry == nil {
-		dataSourceRegistry = make(map[string]func(context.Context) (tfsdk.DataSourceType, error))
+		dataSourceRegistry = make(map[string]func(context.Context) (provider.DataSourceType, error))
 	}
 	dataSourceRegistry[name] = factory
 }
 
 // AddResourceTypeFactory registers the specified resource type name and factory.
-func AddResourceTypeFactory(name string, factory func(context.Context) (tfsdk.ResourceType, error)) {
+func AddResourceTypeFactory(name string, factory func(context.Context) (provider.ResourceType, error)) {
 	resourceRegistryMu.Lock()
 	defer resourceRegistryMu.Unlock()
 
@@ -40,14 +40,14 @@ func AddResourceTypeFactory(name string, factory func(context.Context) (tfsdk.Re
 	}
 
 	if resourceRegistry == nil {
-		resourceRegistry = make(map[string]func(context.Context) (tfsdk.ResourceType, error))
+		resourceRegistry = make(map[string]func(context.Context) (provider.ResourceType, error))
 	}
 	resourceRegistry[name] = factory
 }
 
 // ResourceFactories returns the registered resource factories.
 // Resource registration is closed.
-func ResourceFactories() map[string]func(context.Context) (tfsdk.ResourceType, error) {
+func ResourceFactories() map[string]func(context.Context) (provider.ResourceType, error) {
 	resourceRegistryMu.Lock()
 	defer resourceRegistryMu.Unlock()
 
@@ -58,7 +58,7 @@ func ResourceFactories() map[string]func(context.Context) (tfsdk.ResourceType, e
 
 // DataSourceFactories returns the registered data source factories.
 // Data Source registration is closed.
-func DataSourceFactories() map[string]func(context.Context) (tfsdk.DataSourceType, error) {
+func DataSourceFactories() map[string]func(context.Context) (provider.DataSourceType, error) {
 	dataSourceRegistryMu.Lock()
 	defer dataSourceRegistryMu.Unlock()
 
