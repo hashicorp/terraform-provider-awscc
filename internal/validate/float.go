089ecd517ce5aa77a24dad2daa8e1c265fb1fca7
Kit Ewbank <Kit_Ewbank@hotmail.com>
2022-12-19 14:13:37 -0500

    Revert "Remove float, int and string validators."
    This reverts commit 54a3c18d06f9fe9a96457fd23bfb6987a1e34403.

---
+++ internal/validate/float.go
@@ -0,0 +1,163 @@
+package validate
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-awscc/internal/diag"
+)
+
+// floatBetweenValidator validates that an float Attribute's value is in a range.
+type floatBetweenValidator struct {
+	min, max float64
+}
+
+// Description describes the validation in plain text formatting.
+func (validator floatBetweenValidator) Description(_ context.Context) string {
+	return fmt.Sprintf("value must be between %f and %f", validator.min, validator.max)
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (validator floatBetweenValidator) MarkdownDescription(ctx context.Context) string {
+	return validator.Description(ctx)
+}
+
+// Validate performs the validation.
+func (validator floatBetweenValidator) Validate(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) {
+	f, ok := validateFloat(ctx, request, response)
+	if !ok {
+		return
+	}
+
+	if f < validator.min || f > validator.max {
+		response.Diagnostics.Append(diag.NewInvalidValueAttributeError(
+			request.AttributePath,
+			fmt.Sprintf("expected value to be in the range [%f, %f], got %f", validator.min, validator.max, f),
+		))
+
+		return
+	}
+}
+
+// FloatBetween returns a new float value between validator.
+func FloatBetween(min, max float64) tfsdk.AttributeValidator {
+	if min > max {
+		return nil
+	}
+
+	return floatBetweenValidator{
+		min: min,
+		max: max,
+	}
+}
+
+// floatAtLeastValidator validates that an float Attribute's value is at least a certain value.
+type floatAtLeastValidator struct {
+	min float64
+}
+
+// Description describes the validation in plain text formatting.
+func (validator floatAtLeastValidator) Description(_ context.Context) string {
+	return fmt.Sprintf("value must be at least %f", validator.min)
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (validator floatAtLeastValidator) MarkdownDescription(ctx context.Context) string {
+	return validator.Description(ctx)
+}
+
+// Validate performs the validation.
+func (validator floatAtLeastValidator) Validate(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) {
+	f, ok := validateFloat(ctx, request, response)
+	if !ok {
+		return
+	}
+
+	if f < validator.min {
+		response.Diagnostics.Append(diag.NewInvalidValueAttributeError(
+			request.AttributePath,
+			fmt.Sprintf("expected value to be at least %f, got %f", validator.min, f),
+		))
+
+		return
+	}
+}
+
+// FloatAtLeast returns a new float value at least validator.
+func FloatAtLeast(min float64) tfsdk.AttributeValidator {
+	return floatAtLeastValidator{
+		min: min,
+	}
+}
+
+// floatAtMostValidator validates that an float Attribute's value is at most a certain value.
+type floatAtMostValidator struct {
+	max float64
+}
+
+// Description describes the validation in plain text formatting.
+func (validator floatAtMostValidator) Description(_ context.Context) string {
+	return fmt.Sprintf("value must be at most %f", validator.max)
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (validator floatAtMostValidator) MarkdownDescription(ctx context.Context) string {
+	return validator.Description(ctx)
+}
+
+// Validate performs the validation.
+func (validator floatAtMostValidator) Validate(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) {
+	f, ok := validateFloat(ctx, request, response)
+	if !ok {
+		return
+	}
+
+	if f > validator.max {
+		response.Diagnostics.Append(diag.NewInvalidValueAttributeError(
+			request.AttributePath,
+			fmt.Sprintf("expected value to be at most %f, got %f", validator.max, f),
+		))
+
+		return
+	}
+}
+
+// FloatAtMost returns a new float value at nost validator.
+func FloatAtMost(max float64) tfsdk.AttributeValidator {
+	return floatAtMostValidator{
+		max: max,
+	}
+}
+
+func validateFloat(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) (float64, bool) {
+	var n types.Float64
+
+	diags := tfsdk.ValueAs(ctx, request.AttributeConfig, &n)
+
+	if diags.HasError() {
+		var n types.Number
+
+		diags := tfsdk.ValueAs(ctx, request.AttributeConfig, &n)
+
+		if diags.HasError() {
+			response.Diagnostics = append(response.Diagnostics, diags...)
+
+			return 0, false
+		} else {
+			if n.IsNull() || n.IsUnknown() {
+				return 0, false
+			}
+			f, _ := n.ValueBigFloat().Float64()
+
+			return f, true
+		}
+	} else {
+		if n.IsNull() || n.IsUnknown() {
+			return 0, false
+		}
+
+		return n.ValueFloat64(), true
+	}
+}
---
+++ internal/validate/float_test.go
@@ -0,0 +1,315 @@
+package validate
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/hashicorp/terraform-provider-awscc/internal/tfresource"
+)
+
+func TestFloatBetweenValidator(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val         tftypes.Value
+		f           func(context.Context, tftypes.Value) (attr.Value, error)
+		min         float64
+		max         float64
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"not a number": {
+			val:         tftypes.NewValue(tftypes.Bool, true),
+			f:           types.BoolType.ValueFromTerraform,
+			expectError: true,
+		},
+		"unknown number": {
+			val: tftypes.NewValue(tftypes.Number, tftypes.UnknownValue),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"null number": {
+			val: tftypes.NewValue(tftypes.Number, nil),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid integer as Number": {
+			val: tftypes.NewValue(tftypes.Number, 2),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid integer as Float64": {
+			val: tftypes.NewValue(tftypes.Number, 2),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid float as Number": {
+			val: tftypes.NewValue(tftypes.Number, 2.2),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid float as Float64": {
+			val: tftypes.NewValue(tftypes.Number, 2.2),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid float as Number min": {
+			val: tftypes.NewValue(tftypes.Number, 0.9),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid float as Float64 min": {
+			val: tftypes.NewValue(tftypes.Number, 0.9),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid float as Number max": {
+			val: tftypes.NewValue(tftypes.Number, 3.10),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"valid float as Float64 max": {
+			val: tftypes.NewValue(tftypes.Number, 3.10),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+			max: 3.10,
+		},
+		"too small float as Number": {
+			val:         tftypes.NewValue(tftypes.Number, -1.1111),
+			f:           types.NumberType.ValueFromTerraform,
+			min:         0.90,
+			max:         3.10,
+			expectError: true,
+		},
+		"too large float as Number": {
+			val:         tftypes.NewValue(tftypes.Number, 4.2),
+			f:           types.NumberType.ValueFromTerraform,
+			min:         0.90,
+			max:         3.10,
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			val, err := test.f(ctx, test.val)
+
+			if err != nil {
+				t.Fatalf("got unexpected error: %s", err)
+			}
+
+			request := tfsdk.ValidateAttributeRequest{
+				AttributePath:   path.Root("test"),
+				AttributeConfig: val,
+			}
+			response := tfsdk.ValidateAttributeResponse{}
+			FloatBetween(test.min, test.max).Validate(ctx, request, &response)
+
+			if !response.Diagnostics.HasError() && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if response.Diagnostics.HasError() && !test.expectError {
+				t.Fatalf("got unexpected error: %s", tfresource.DiagsError(response.Diagnostics))
+			}
+		})
+	}
+}
+
+func TestFloatAtLeastValidator(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val         tftypes.Value
+		f           func(context.Context, tftypes.Value) (attr.Value, error)
+		min         float64
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"not a number": {
+			val:         tftypes.NewValue(tftypes.Bool, true),
+			f:           types.BoolType.ValueFromTerraform,
+			expectError: true,
+		},
+		"unknown number": {
+			val: tftypes.NewValue(tftypes.Number, tftypes.UnknownValue),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+		},
+		"null number": {
+			val: tftypes.NewValue(tftypes.Number, nil),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+		},
+		"valid integer as Number": {
+			val: tftypes.NewValue(tftypes.Number, 2),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+		},
+		"valid integer as Float64": {
+			val: tftypes.NewValue(tftypes.Number, 2),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+		},
+		"valid float as Number": {
+			val: tftypes.NewValue(tftypes.Number, 2.2),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+		},
+		"valid float as Float64": {
+			val: tftypes.NewValue(tftypes.Number, 2.2),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+		},
+		"valid float as Number min": {
+			val: tftypes.NewValue(tftypes.Number, 0.9),
+			f:   types.NumberType.ValueFromTerraform,
+			min: 0.90,
+		},
+		"valid float as Float64 min": {
+			val: tftypes.NewValue(tftypes.Number, 0.9),
+			f:   types.Float64Type.ValueFromTerraform,
+			min: 0.90,
+		},
+		"too small float as Number": {
+			val:         tftypes.NewValue(tftypes.Number, -1.1111),
+			f:           types.NumberType.ValueFromTerraform,
+			min:         0.90,
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			val, err := test.f(ctx, test.val)
+
+			if err != nil {
+				t.Fatalf("got unexpected error: %s", err)
+			}
+
+			request := tfsdk.ValidateAttributeRequest{
+				AttributePath:   path.Root("test"),
+				AttributeConfig: val,
+			}
+			response := tfsdk.ValidateAttributeResponse{}
+			FloatAtLeast(test.min).Validate(ctx, request, &response)
+
+			if !response.Diagnostics.HasError() && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if response.Diagnostics.HasError() && !test.expectError {
+				t.Fatalf("got unexpected error: %s", tfresource.DiagsError(response.Diagnostics))
+			}
+		})
+	}
+}
+
+func TestFloatAtMostValidator(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val         tftypes.Value
+		f           func(context.Context, tftypes.Value) (attr.Value, error)
+		max         float64
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"not a number": {
+			val:         tftypes.NewValue(tftypes.Bool, true),
+			f:           types.BoolType.ValueFromTerraform,
+			expectError: true,
+		},
+		"unknown number": {
+			val: tftypes.NewValue(tftypes.Number, tftypes.UnknownValue),
+			f:   types.NumberType.ValueFromTerraform,
+			max: 2.00,
+		},
+		"null number": {
+			val: tftypes.NewValue(tftypes.Number, nil),
+			f:   types.NumberType.ValueFromTerraform,
+			max: 2.00,
+		},
+		"valid integer as Number": {
+			val: tftypes.NewValue(tftypes.Number, 1),
+			f:   types.NumberType.ValueFromTerraform,
+			max: 2.00,
+		},
+		"valid integer as Float64": {
+			val: tftypes.NewValue(tftypes.Number, 1),
+			f:   types.Float64Type.ValueFromTerraform,
+			max: 2.00,
+		},
+		"valid float as Number": {
+			val: tftypes.NewValue(tftypes.Number, 1.1),
+			f:   types.NumberType.ValueFromTerraform,
+			max: 2.00,
+		},
+		"valid float as Float64": {
+			val: tftypes.NewValue(tftypes.Number, 1.1),
+			f:   types.Float64Type.ValueFromTerraform,
+			max: 2.00,
+		},
+		"valid float as Number max": {
+			val: tftypes.NewValue(tftypes.Number, 2.00),
+			f:   types.NumberType.ValueFromTerraform,
+			max: 2.00,
+		},
+		"valid float as Float64 max": {
+			val: tftypes.NewValue(tftypes.Number, 2.00),
+			f:   types.Float64Type.ValueFromTerraform,
+			max: 2.00,
+		},
+		"too large float as Number": {
+			val:         tftypes.NewValue(tftypes.Number, 3.00),
+			f:           types.NumberType.ValueFromTerraform,
+			max:         2.00,
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			val, err := test.f(ctx, test.val)
+
+			if err != nil {
+				t.Fatalf("got unexpected error: %s", err)
+			}
+
+			request := tfsdk.ValidateAttributeRequest{
+				AttributePath:   path.Root("test"),
+				AttributeConfig: val,
+			}
+			response := tfsdk.ValidateAttributeResponse{}
+			FloatAtMost(test.max).Validate(ctx, request, &response)
+
+			if !response.Diagnostics.HasError() && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if response.Diagnostics.HasError() && !test.expectError {
+				t.Fatalf("got unexpected error: %s", tfresource.DiagsError(response.Diagnostics))
+			}
+		})
+	}
+}
