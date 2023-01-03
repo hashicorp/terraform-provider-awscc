2be89bb7a27302ddc616eb52c5c61268060934e0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-03 14:05:57 -0500

    Regenerate resources.

--- internal/aws/logs/log_group_resource_gen.go
+++ internal/aws/logs/log_group_resource_gen.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
