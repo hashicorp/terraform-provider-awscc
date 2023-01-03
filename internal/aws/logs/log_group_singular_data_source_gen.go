0cc4ff85021b19f12cd13b2cb9c2f12429aaf835
Kit Ewbank <Kit_Ewbank@hotmail.com>
2023-01-03 16:33:41 -0500

    Generate all singular data sources.

--- internal/aws/logs/log_group_singular_data_source_gen.go
+++ internal/aws/logs/log_group_singular_data_source_gen.go
@@ -19,7 +19,7 @@ func init() {
 // logGroupDataSource returns the Terraform awscc_logs_log_group data source.
 // This Terraform data source corresponds to the CloudFormation AWS::Logs::LogGroup resource.
 func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
-	attributes := map[string]schema.Attribute{
+	attributes := map[string]schema.Attribute{ /*START SCHEMA*/
 		// Property: Arn
 		// CloudFormation resource type schema:
 		//
@@ -27,10 +27,10 @@ func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
 		//	  "description": "The CloudWatch log group ARN.",
 		//	  "type": "string"
 		//	}
-		"arn": schema.StringAttribute{
+		"arn": schema.StringAttribute{ /*START ATTRIBUTE*/
 			Description: "The CloudWatch log group ARN.",
 			Computed:    true,
-		},
+		}, /*END ATTRIBUTE*/
 		// Property: DataProtectionPolicy
 		// CloudFormation resource type schema:
 		//
@@ -38,11 +38,11 @@ func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
 		//	  "description": "The body of the policy document you want to use for this topic.\n\nYou can only add one policy per topic.\n\nThe policy must be in JSON string format.\n\nLength Constraints: Maximum length of 30720",
 		//	  "type": "object"
 		//	}
-		"data_protection_policy": schema.MapAttribute{
+		"data_protection_policy": schema.MapAttribute{ /*START ATTRIBUTE*/
 			ElementType: types.StringType,
 			Description: "The body of the policy document you want to use for this topic.\n\nYou can only add one policy per topic.\n\nThe policy must be in JSON string format.\n\nLength Constraints: Maximum length of 30720",
 			Computed:    true,
-		},
+		}, /*END ATTRIBUTE*/
 		// Property: KmsKeyId
 		// CloudFormation resource type schema:
 		//
@@ -52,10 +52,10 @@ func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
 		//	  "pattern": "",
 		//	  "type": "string"
 		//	}
-		"kms_key_id": schema.StringAttribute{
+		"kms_key_id": schema.StringAttribute{ /*START ATTRIBUTE*/
 			Description: "The Amazon Resource Name (ARN) of the CMK to use when encrypting log data.",
 			Computed:    true,
-		},
+		}, /*END ATTRIBUTE*/
 		// Property: LogGroupName
 		// CloudFormation resource type schema:
 		//
@@ -66,10 +66,10 @@ func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
 		//	  "pattern": "",
 		//	  "type": "string"
 		//	}
-		"log_group_name": schema.StringAttribute{
+		"log_group_name": schema.StringAttribute{ /*START ATTRIBUTE*/
 			Description: "The name of the log group. If you don't specify a name, AWS CloudFormation generates a unique ID for the log group.",
 			Computed:    true,
-		},
+		}, /*END ATTRIBUTE*/
 		// Property: RetentionInDays
 		// CloudFormation resource type schema:
 		//
@@ -100,10 +100,10 @@ func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
 		//	  ],
 		//	  "type": "integer"
 		//	}
-		"retention_in_days": schema.Int64Attribute{
+		"retention_in_days": schema.Int64Attribute{ /*START ATTRIBUTE*/
 			Description: "The number of days to retain the log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653.",
 			Computed:    true,
-		},
+		}, /*END ATTRIBUTE*/
 		// Property: Tags
 		// CloudFormation resource type schema:
 		//
@@ -136,25 +136,25 @@ func logGroupDataSource(ctx context.Context) (datasource.DataSource, error) {
 		//	  "type": "array",
 		//	  "uniqueItems": true
 		//	}
-		"tags": schema.SetNestedAttribute{
-			NestedObject: schema.NestedAttributeObject{
-				Attributes: map[string]schema.Attribute{
+		"tags": schema.SetNestedAttribute{ /*START ATTRIBUTE*/
+			NestedObject: schema.NestedAttributeObject{ /*START NESTED OBJECT*/
+				Attributes: map[string]schema.Attribute{ /*START SCHEMA*/
 					// Property: Key
-					"key": schema.StringAttribute{
+					"key": schema.StringAttribute{ /*START ATTRIBUTE*/
 						Description: "The key name of the tag. You can specify a value that is 1 to 128 Unicode characters in length and cannot be prefixed with aws:. You can use any of the following characters: the set of Unicode letters, digits, whitespace, _, ., :, /, =, +, - and @.",
 						Computed:    true,
-					},
+					}, /*END ATTRIBUTE*/
 					// Property: Value
-					"value": schema.StringAttribute{
+					"value": schema.StringAttribute{ /*START ATTRIBUTE*/
 						Description: "The value for the tag. You can specify a value that is 0 to 256 Unicode characters in length. You can use any of the following characters: the set of Unicode letters, digits, whitespace, _, ., :, /, =, +, - and @.",
 						Computed:    true,
-					},
-				},
-			},
+					}, /*END ATTRIBUTE*/
+				}, /*END SCHEMA*/
+			}, /*END NESTED OBJECT*/
 			Description: "An array of key-value pairs to apply to this resource.",
 			Computed:    true,
-		},
-	}
+		}, /*END ATTRIBUTE*/
+	} /*END SCHEMA*/
 
 	attributes["id"] = schema.StringAttribute{
 		Description: "Uniquely identifies the resource.",
