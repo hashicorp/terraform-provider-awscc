620c4c5685f28022a0b10c51215d6466fe806081
Jacky Wong <jackylw@amazon.com>
2023-05-04 23:36:42 +0800

    docs: add example for:ecr_repository

---
+++ examples/resources/awscc_ecr_repository/ecr_repository_repository_policy.tf
@@ -0,0 +1,22 @@
+resource "awscc_ecr_repository" "repo_policy_example" {
+  repository_name      = "example-ecr-repositry-repo"
+  image_tag_mutability = "MUTABLE"
+
+  repository_policy_text = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "DenyPull",
+          "Effect" : "Deny",
+          "Principal" : "*",
+          "Action" : [
+            "ecr:BatchGetImage",
+            "ecr:GetDownloadUrlForLayer"
+          ]
+        }
+      ]
+    }
+  )
+
+}
