56a9d7fef26f45912fa537ebff6a216a6288edbb
Troy Parrett <parrew@amazon.com>
2023-05-09 19:39:39 +0000

    opensearch access policy

---
+++ examples/resources/awscc_opensearchserverless_access_policy/access_policy.tf
@@ -0,0 +1,29 @@
+resource "awscc_opensearchserverless_access_policy" "os" {
+  name = "test-os-security-policy"
+  policy = jsonencode([{
+    "Description" = "Access for test-user",
+    "Rules" = [
+      {
+        "ResourceType" = "index",
+        "Resource" = [
+          "index/*/*"
+        ],
+        "Permission" = [
+          "aoss:*"
+        ]
+        }, {
+        "ResourceType" = "collection",
+        "Resource" = [
+          "collection/my-collection"
+        ],
+        "Permission" = [
+          "aoss:*"
+        ]
+    }],
+    "Principal" = [
+      "arn:aws:iam::111122223333:user/test-user"
+    ]
+  }])
+  type        = "data"
+  description = "Access for test-user"
+}
