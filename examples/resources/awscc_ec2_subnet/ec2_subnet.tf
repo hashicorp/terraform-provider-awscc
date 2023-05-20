63f089eec530cf8e902c627decbecf6bce5ac74b
thiagarr <thiagarr@amazon.com>
2023-05-20 15:24:36 +0000

    Updated to remove advanced example

--- examples/resources/awscc_ec2_subnet/ec2_subnet.tf
+++ examples/resources/awscc_ec2_subnet/ec2_subnet.tf
@@ -1,9 +1,13 @@
+resource "awscc_ec2_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "awscc_ec2_subnet" "main" {
   vpc_id     = resource.awscc_ec2_vpc.main.id
   cidr_block = "10.0.1.0/24"
   tags = [{
-    key   = "Name"
-    value = "main"
+    key   = "Modified By"
+    value = "AWSCC"
   }]
 }
 
