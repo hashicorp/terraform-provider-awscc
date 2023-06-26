f2d77042dc5e92e57c1cb41baa4adbddb6ec09e9
Jacky Wong <jackylw@amazon.com>
2023-06-26 20:44:41 +0800

    add examples for secretsmanager_secret

---
+++ examples/resources/awscc_secretsmanager_secret/secretsmanager_replica_regions.tf
@@ -0,0 +1,10 @@
+resource "awscc_secretsmanager_secret" "example_replica" {
+  name = "example_replica"
+
+  replica_regions = [{
+    region = "ap-southeast-1"
+  },
+  {
+    region = "ap-southeast-2"
+  }]
+}
---
+++ examples/resources/awscc_secretsmanager_secret/secretsmanager_secret.tf
@@ -0,0 +1,4 @@
+resource "awscc_secretsmanager_secret" "example" {
+  name = "example"
+  description = "this is a user-provided description of the secret."
+}
