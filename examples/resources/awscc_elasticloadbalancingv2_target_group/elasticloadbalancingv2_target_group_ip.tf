9bec47cd85103334e876ddf4d20620fbeb7afaaa
Bonnie Ng <nim.1111.ou@gmail.com>
2023-07-05 17:12:16 +0000

    docs: add example for: elasticloadbalancingv2_target_group

---
+++ examples/resources/awscc_elasticloadbalancingv2_target_group/elasticloadbalancingv2_target_group_ip.tf
@@ -0,0 +1,11 @@
+resource "awscc_elasticloadbalancingv2_target_group" "ip-example" {
+  name     = "ip-example"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = awscc_ec2_vpc.main.id
+  tags = [{
+    key   = "Modified By"
+    value = "AWSCC"
+  }]
+}
---
+++ examples/resources/awscc_elasticloadbalancingv2_target_group/elasticloadbalancingv2_target_group_lambda.tf
@@ -0,0 +1,8 @@
+resource "awscc_elasticloadbalancingv2_target_group" "lambda-example" {
+  name     = "lambda-example"
+  target_type = "lambda"
+  tags = [{
+    key   = "Modified By"
+    value = "AWSCC"
+  }]
+}
