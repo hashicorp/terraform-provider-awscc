897b234186543e7db1fb8b1e94116b7410434a5a
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-08-10 12:58:30 -0500

    add initial teamcity config

---
+++ .teamcity/components/build_config.kt
@@ -0,0 +1,11 @@
+import jetbrains.buildServer.configs.kotlin.BuildSteps
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import java.io.File
+
+fun BuildSteps.ConfigureGoEnv() {
+    step(ScriptBuildStep {
+        name = "Configure GOENV"
+        scriptContent = File("./scripts/configure_goenv.sh").readText()
+    })
+}
---
+++ .teamcity/scripts/acceptance_tests.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+echo "$GOPATH"
+goenv versions
+echo "go env:"
+go env
+
+# shellcheck disable=2157 # These aren't constant strings, they're TeamCity variable substitution
+if [[ -n "%ACCTEST_ROLE_ARN%" || -n "%ACCTEST_ALTERNATE_ROLE_ARN%" ]]; then
+	conf=$(pwd)/aws.conf
+
+	function cleanup {
+		rm "${conf}"
+	}
+	trap cleanup EXIT
+
+	touch "${conf}"
+	chmod 600 "${conf}"
+
+	export AWS_CONFIG_FILE="${conf}"
+
+	# shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
+	if [[ -n "%ACCTEST_ROLE_ARN%" ]]; then
+		cat <<EOF >>"${conf}"
+[profile primary]
+role_arn       = %ACCTEST_ROLE_ARN%
+source_profile = primary_user
+
+[profile primary_user]
+aws_access_key_id     = %AWS_ACCESS_KEY_ID%
+aws_secret_access_key = %AWS_SECRET_ACCESS_KEY%
+EOF
+
+		unset AWS_ACCESS_KEY_ID
+		unset AWS_SECRET_ACCESS_KEY
+
+		export AWS_PROFILE=primary
+	fi
+
+	# shellcheck disable=2157 # This isn't a constant string, it's a TeamCity variable substitution
+	if [[ -n "%ACCTEST_ALTERNATE_ROLE_ARN%" ]]; then
+		cat <<EOF >>"${conf}"
+[profile alternate]
+role_arn       = %ACCTEST_ALTERNATE_ROLE_ARN%
+source_profile = alternate_user
+
+[profile alternate_user]
+aws_access_key_id     = %AWS_ALTERNATE_ACCESS_KEY_ID%
+aws_secret_access_key = %AWS_ALTERNATE_SECRET_ACCESS_KEY%
+EOF
+
+		unset AWS_ALTERNATE_ACCESS_KEY_ID
+		unset AWS_ALTERNATE_SECRET_ACCESS_KEY
+
+		export AWS_ALTERNATE_PROFILE=alternate
+	fi
+fi
+
+readonly TEST_BINARY='./test-binary'
+
+# We need to loop here, since `go test -c` only allows a single package at a time
+for DIR in $(find ./internal/aws/ -type d | sort -u); do
+    pushd "$DIR" || continue
+
+    go test -c . -o $TEST_BINARY
+    TEST_LIST=$($TEST_BINARY -test.list="%TEST_PATTERN%")
+    TEST_COUNT=$(echo "${TEST_LIST}" | wc -l)
+    echo "Running ${TEST_COUNT} tests in ${DIR}..."
+    echo "${TEST_LIST}" | TF_ACC=1 teamcity-go-test -test $TEST_BINARY -parallelism %ACCTEST_PARALLELISM%
+
+    popd || continue
+done
