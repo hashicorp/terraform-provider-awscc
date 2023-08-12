14e1c843a5ba195365af630c20f5f4900a66a679
GlennChia <glennchia7@gmail.com>
2023-08-12 14:28:30 +0000

    docs: ec2_ipam_allocation

---
+++ examples/resources/awscc_ec2_ipam_allocation/cidr.tf
@@ -0,0 +1,28 @@
+data "aws_region" "current" {}
+
+resource "awscc_ec2_ipam" "example" {
+  operating_regions = [
+    {
+      region_name = data.aws_region.current.name
+    }
+  ]
+}
+
+resource "awscc_ec2_ipam_pool" "example" {
+  address_family = "ipv4"
+  ipam_scope_id  = awscc_ec2_ipam.example.private_default_scope_id
+  locale         = data.aws_region.current.name
+
+  provisioned_cidrs = [
+    {
+      cidr = "10.0.0.0/16"
+    }
+  ]
+}
+
+resource "awscc_ec2_ipam_allocation" "example" {
+  ipam_pool_id = awscc_ec2_ipam_pool.example.id
+  description  = "reserved using cidr"
+
+  cidr = "10.0.0.0/32"
+}
