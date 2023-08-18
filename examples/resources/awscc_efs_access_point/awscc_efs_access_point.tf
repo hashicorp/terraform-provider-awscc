df58583fb2a42324201f0a54a3caba3f47f33231
ichautha <ichautha@amazon.com>
2023-08-18 22:09:22 +0530

    Added efs resource creation part and dynamic reference

--- examples/resources/awscc_efs_access_point/awscc_efs_access_point.tf
+++ examples/resources/awscc_efs_access_point/awscc_efs_access_point.tf
@@ -12,3 +12,17 @@ resource "awscc_efs_access_point" "this" {
     }
   ]
 }
+
+resource "awscc_efs_file_system" "this" {
+
+  file_system_tags = [
+    {
+      key   = "Name"
+      value = "this"
+    },
+    {
+      key   = "Modified By"
+      value = "AWSCC"
+    }
+  ]
+}
--- examples/resources/awscc_efs_access_point/awscc_efs_access_point_posix_user.tf
+++ examples/resources/awscc_efs_access_point/awscc_efs_access_point_posix_user.tf
@@ -1,11 +1,25 @@
 resource "awscc_efs_access_point" "this" {
-  file_system_id = "fs-0dbbfe225fc860016" #awscc_efs_file_system.this.id
+  file_system_id = awscc_efs_file_system.this.id
   posix_user = {
     gid = 0
     uid = 0
   }
 
   access_point_tags = [
+    {
+      key   = "Name"
+      value = "this"
+    },
+    {
+      key   = "Modified By"
+      value = "AWSCC"
+    }
+  ]
+}
+
+resource "awscc_efs_file_system" "this" {
+
+  file_system_tags = [
     {
       key   = "Name"
       value = "this"
