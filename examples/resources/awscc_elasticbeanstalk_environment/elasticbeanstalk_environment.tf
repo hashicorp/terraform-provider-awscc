6f5b4379e829e6db28ce108242bf017d457a2e75
Bonnie Ng <nim.1111.ou@gmail.com>
2023-08-18 07:51:20 +0000

    docs: add example for: elasticbeanstalk_environment

---
+++ examples/resources/awscc_elasticbeanstalk_environment/elasticbeanstalk_environment.tf
@@ -0,0 +1,18 @@
+resource "awscc_elasticbeanstalk_application" "example-app" {
+  application_name = "example-app"
+  description      = "example-app"
+}
+
+resource "awscc_elasticbeanstalk_environment" "example-env" {
+  application_name    = awscc_elasticbeanstalk_application.example-app.application_name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.2 running Python 3.11"
+  option_settings = [{
+    namespace   = "aws:autoscaling:launchconfiguration"
+    option_name = "IamInstanceProfile"
+    value       = "example-aws-elasticbeanstalk-ec2-role"
+  }]
+  tags = [{
+    key   = "Managed By"
+    value = "AWSCC"
+  }]
+}
