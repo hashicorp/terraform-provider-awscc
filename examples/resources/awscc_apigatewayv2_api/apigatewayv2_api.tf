cbb9182680a01582de8206bd53b3ca8840568527
robinrz <robinrz@amazon.com>
2023-09-02 22:23:31 +0000

    docs: add example for: apigatewayv2_api

---
+++ examples/resources/awscc_apigatewayv2_api/apigatewayv2_api.tf
@@ -0,0 +1,12 @@
+resource "awscc_apigatewayv2_api" "example_api" {
+  name                       = "example-websocket-api"
+  protocol_type              = "WEBSOCKET"
+  route_selection_expression = "$request.body.action"
+  tags = {
+    key = "Modified By"
+    value = "AWSCC"
+  }
+}
+
+
+
