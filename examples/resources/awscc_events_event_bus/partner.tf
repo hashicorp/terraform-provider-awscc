d9f53e3ca91ff29bcb1e89aa8e2c516f9b0f65dc
wellsiau-aws <wellsiau@amazon.com>
2023-12-21 16:08:31 -0800

    doc: awscc_events_event_bus

---
+++ examples/resources/awscc_events_event_bus/partner.tf
@@ -0,0 +1,4 @@
+resource "awscc_events_event_bus" "sample_partner_event_bus" {
+  name = "aws.partner/examplepartner.com/example-eqrry6br"
+  event_source_name = "aws.partner/examplepartner.com/example-eqrry6br"
+}
