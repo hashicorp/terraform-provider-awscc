bc77e5599726b993d8a427e3e23d2a1d9e277004
gankidis <gankidis@amazon.com>
2024-04-05 14:38:05 -0400

    d-improve-greengrassv2_component_version

---
+++ examples/resources/awscc_greengrassv2_component_version/greengrassv2_component_version.tf
@@ -0,0 +1,29 @@
+resource "awscc_greengrassv2_component_version" "MyGreengrassComponentVersion_example" {
+  inline_recipe = jsonencode({
+    "RecipeFormatVersion"     = "2020-01-25"
+    "ComponentName"           = "MyLambdaComponent"
+    "ComponentVersion"        = "1.0.0"
+    "ComponentDescription"    = "This is a sample Greengrass component created using InlineRecipe."
+    "ComponentPublisher"      = "MyCompany"
+    "Manifests" = [
+      {
+        "Platform" = {
+          "os"   = "linux"
+          "arch" = "armhf"
+        }
+        "Lifecycle" = {
+          "install" = {
+            "script" = "apt-get install -y my-package"
+          }
+          "run" = {
+            "script" = "python3 my_script.py"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    MyTagKey = "MyTagValue"
+  }
+}
