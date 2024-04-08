95471f6f1548e9d6f3e932999c49919a20b23ea1
gokendra1 <109332493+gokendra1@users.noreply.github.com>
2024-04-08 23:12:11 +0530

    Update examples/resources/awscc_controltower_enabled_control/controltower_enabled_control.tf
    Co-authored-by: Welly Siauw AWS <67753754+wellsiau-aws@users.noreply.github.com>

--- examples/resources/awscc_controltower_enabled_control/controltower_enabled_control.tf
+++ examples/resources/awscc_controltower_enabled_control/controltower_enabled_control.tf
@@ -5,7 +5,7 @@
 
 resource "awscc_controltower_enabled_control" "example" {
   control_identifier = "arn:aws:controltower:us-east-1::control/AWS-GR_AUDIT_BUCKET_RETENTION_POLICY"
-  target_identifier  = "arn:aws:organizations::000123456789:ou/o-ydkk7uvfj1/ou-e3wm-2x760yqd"
+  target_identifier  = "arn:aws:organizations::<<your-account-id>:ou/<<your-org-id>>/<<your-ou-id>>"
 }
 
 # Please change the Organization ID from 'o-ydkk7uvfj1' to your Organization ID, and the Organizational Unit ID from 'u-e3wm-2x760yqd' to your desired OU where the controls need to be implemented.
