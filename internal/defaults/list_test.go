a3183737d7ba50c5766c0dc7cb4c04e6619c335f
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-01 14:12:49 -0400

    Test 'defaults.EmptyListNestedObject' and 'defaults.EmptySetNestedObject'.

--- internal/defaults/list_test.go
+++ internal/defaults/list_test.go
@@ -7,7 +7,12 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/defaults"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func TestStaticListOfString(t *testing.T) {
@@ -23,5 +28,68 @@ func TestStaticListOfString(t *testing.T) {
 	}
 	if want, got := 2, len(response.PlanValue.Elements()); got != want {
 		t.Errorf("StaticListOfString.PlanValue length %d, want %d", got, want)
+	}
+}
+
+func TestEmptyListNestedObject(t *testing.T) {
+	t.Parallel()
+
+	attributeTypes := map[string]attr.Type{
+		"name": types.StringType,
+		"size": types.Int64Type,
+	}
+
+	type testCase struct {
+		plannedValue  types.List
+		expectedValue types.List
+	}
+	tests := map[string]testCase{
+		"null list": {
+			plannedValue:  types.ListNull(types.ObjectType{AttrTypes: attributeTypes}),
+			expectedValue: types.ListValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{}),
+		},
+		"empty list": {
+			plannedValue:  types.ListValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{}),
+			expectedValue: types.ListValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{}),
+		},
+		"non-empty list": {
+			plannedValue: types.ListValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n1"),
+					"size": types.Int64Value(1),
+				}),
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n2"),
+					"size": types.Int64Value(2),
+				}),
+			}),
+			expectedValue: types.ListValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n1"),
+					"size": types.Int64Value(1),
+				}),
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n2"),
+					"size": types.Int64Value(2),
+				}),
+			}),
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			request := planmodifier.ListRequest{
+				Path:      path.Root("test"),
+				PlanValue: test.plannedValue,
+			}
+			response := planmodifier.ListResponse{}
+			EmptyListNestedObject().PlanModifyList(ctx, request, &response)
+
+			if diff := cmp.Diff(response.PlanValue, test.expectedValue); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
 	}
 }
--- internal/defaults/set_test.go
+++ internal/defaults/set_test.go
@@ -7,7 +7,12 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/defaults"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func TestStaticSetOfString(t *testing.T) {
@@ -23,5 +28,68 @@ func TestStaticSetOfString(t *testing.T) {
 	}
 	if want, got := 2, len(response.PlanValue.Elements()); got != want {
 		t.Errorf("StaticListOfString.PlanValue length %d, want %d", got, want)
+	}
+}
+
+func TestEmptySetNestedObject(t *testing.T) {
+	t.Parallel()
+
+	attributeTypes := map[string]attr.Type{
+		"name": types.StringType,
+		"size": types.Int64Type,
+	}
+
+	type testCase struct {
+		plannedValue  types.Set
+		expectedValue types.Set
+	}
+	tests := map[string]testCase{
+		"null list": {
+			plannedValue:  types.SetNull(types.ObjectType{AttrTypes: attributeTypes}),
+			expectedValue: types.SetValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{}),
+		},
+		"empty list": {
+			plannedValue:  types.SetValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{}),
+			expectedValue: types.SetValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{}),
+		},
+		"non-empty list": {
+			plannedValue: types.SetValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n1"),
+					"size": types.Int64Value(1),
+				}),
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n2"),
+					"size": types.Int64Value(2),
+				}),
+			}),
+			expectedValue: types.SetValueMust(types.ObjectType{AttrTypes: attributeTypes}, []attr.Value{
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n1"),
+					"size": types.Int64Value(1),
+				}),
+				types.ObjectValueMust(attributeTypes, map[string]attr.Value{
+					"name": types.StringValue("n2"),
+					"size": types.Int64Value(2),
+				}),
+			}),
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			ctx := context.TODO()
+			request := planmodifier.SetRequest{
+				Path:      path.Root("test"),
+				PlanValue: test.plannedValue,
+			}
+			response := planmodifier.SetResponse{}
+			EmptySetNestedObject().PlanModifySet(ctx, request, &response)
+
+			if diff := cmp.Diff(response.PlanValue, test.expectedValue); diff != "" {
+				t.Errorf("unexpected diff (+wanted, -got): %s", diff)
+			}
+		})
 	}
 }
