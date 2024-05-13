b862c46b181098aea0c3ef8868a34f11225659f1
vivekthangamuthu <42886870+vivekthangamuthu@users.noreply.github.com>
2024-05-13 10:13:36 +0530

    Update sagemaker_image.tf

--- examples/resources/awscc_sagemaker_image/sagemaker_image.tf
+++ examples/resources/awscc_sagemaker_image/sagemaker_image.tf
@@ -1,4 +1,18 @@
 resource "awscc_sagemaker_image" "example" {
   image_name     = "example"
-  image_role_arn = aws_iam_role.test.arn
+  image_role_arn = awscc_iam_role.main.arn
+  tags = [
+    {
+      key   = "Name"
+      value = "example"
+    },
+    {
+      key   = "Environment"
+      value = "Development"
+    },
+    {
+      key   = "Modified By"
+      value = "AWSCC"
+    }
+  ]
 }
