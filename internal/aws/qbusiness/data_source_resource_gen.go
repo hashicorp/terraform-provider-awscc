ce132cbf1e774b2842541d91b64849c1d2b4e8dc
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-05-20 14:24:37 -0400

    Run 'make resources'.

--- internal/aws/qbusiness/data_source_resource_gen.go
+++ internal/aws/qbusiness/data_source_resource_gen.go
@@ -7,6 +7,8 @@ package qbusiness
 
 import (
 	"context"
+	"regexp"
+
 	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
@@ -22,7 +24,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-awscc/internal/generic"
 	"github.com/hashicorp/terraform-provider-awscc/internal/registry"
-	"regexp"
 )
 
 func init() {
