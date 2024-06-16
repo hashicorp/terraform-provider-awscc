ce3d3d55973657d4f110d6bd453fbfdf62069dab
Anthony Wat <acwwat@gmail.com>
2024-06-15 21:44:50 -0400

    docs: Add examples for awscc_lightsail_database

---
+++ examples/resources/awscc_lightsail_database/mysql.tf
@@ -0,0 +1,7 @@
+resource "awscc_lightsail_database" "example" {
+  master_database_name             = "example"
+  master_username                  = "admin"
+  relational_database_blueprint_id = "mysql_8_0"
+  relational_database_bundle_id    = "micro_2_0"
+  relational_database_name         = "example-db"
+}
