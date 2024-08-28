cd9e9acd3abb0b7ef0164617c9bb13ff81dfd4e5
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-08-28 14:44:03 -0500

    rename file

--- internal/provider/schemas.go
+++ internal/provider/schemas.go
@@ -1,6 +1,6 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:generate go run generators/schema/main.go -config all_schemas.hcl -generated-code-root .. -import-path-root github.com/hashicorp/terraform-provider-awscc/internal -- resources.go singular_data_sources.go plural_data_sources.go import_examples.json
+//go:generate go run generators/schema/main.go -config all_schemas.hcl -generated-code-root .. -import-path-root github.com/hashicorp/terraform-provider-awscc/internal -- resources.go singular_data_sources.go plural_data_sources.go import_examples_gen.json
 
 package provider
