09f38eb8dfc51193d4a2922f490fd60f41090160
wellsiau-aws <wellsiau@amazon.com>
2024-09-06 18:35:40 -0700

    make docs

---
+++ examples/resources/awscc_events_connection/connection_with_api_key_authorizer.tf
@@ -0,0 +1,17 @@
+resource "awscc_events_connection" "pagerduty_connection" {
+  name                = "pagerduty-connection"
+  authorization_type  = "API_KEY"
+
+  auth_parameters = {
+    api_key_auth_parameters = {
+      api_key_name  = "Authorization"
+      api_key_value = "my-secret-string"
+    }
+
+    additional_parameters = {
+      body_parameters = {
+        routing_key = "my-pagerduty-integration-key"
+      }
+    }
+  }
+}
---
+++ examples/resources/awscc_events_connection/connection_with_oauth_authorizer.tf
@@ -0,0 +1,25 @@
+resource "awscc_events_connection" "auth0_connection" {
+  name                = "auth0-connection"
+  authorization_type  = "OAUTH_CLIENT_CREDENTIALS"
+
+  auth_parameters = {
+    o_auth_parameters = {
+      client_parameters = {
+        client_id     = "my-client-id"
+        client_secret = "my-secret-string"
+      }
+
+      authorization_endpoint = "https://yourUserName.us.auth0.com/oauth/token"
+      http_method            = "POST"
+
+      o_auth_http_parameters = {
+        body_parameters = [
+          {
+            key = "audience",
+            value = "my-auth0-identifier"
+          }
+        ]
+      }
+    }
+  }
+}
