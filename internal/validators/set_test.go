cac935d37315f5f389e94f968bdfa0a667bf24ab
Adrian Johnson <adrian.johnson@hashicorp.com>
2024-09-18 15:03:57 -0500

    add test for NotNullSet

---
+++ internal/validators/set_test.go
@@ -0,0 +1,66 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package validators_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	fwvalidators "github.com/hashicorp/terraform-provider-awscc/internal/validators"
+)
+
+func TestNotNullSet(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val                 types.Set
+		expectedDiagnostics diag.Diagnostics
+	}
+	tests := map[string]testCase{
+		"null": {
+			val: types.SetNull(types.StringType),
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewAttributeErrorDiagnostic(
+					path.Root("test"),
+					"Missing Attribute Value",
+					`test: value must be configured`,
+				),
+			},
+		},
+		"unknown": {
+			val: types.SetUnknown(types.StringType),
+		},
+		"valid": {
+			val: types.SetValueMust(types.StringType, []attr.Value{
+				types.StringValue("test"),
+			}),
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+
+			request := validator.SetRequest{
+				Path:           path.Root("test"),
+				PathExpression: path.MatchRoot("test"),
+				ConfigValue:    test.val,
+			}
+			response := validator.SetResponse{}
+			fwvalidators.NotNullSet().ValidateSet(ctx, request, &response)
+
+			if diff := cmp.Diff(response.Diagnostics, test.expectedDiagnostics); diff != "" {
+				t.Errorf("unexpected diagnostics difference: %s", diff)
+			}
+		})
+	}
+}
