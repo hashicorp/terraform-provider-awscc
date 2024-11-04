3a1ee5dca3302a31aa8b8ab1c82588f3ddbea5ee
mehdi-aws <mssalehi@amazon.com>
2024-11-04 19:24:15 +1100

    Use variable for EKS cluster name

--- examples/resources/awscc_eks_identity_provider_config/eks_identity_provider_config_with_auth0.tf
+++ examples/resources/awscc_eks_identity_provider_config/eks_identity_provider_config_with_auth0.tf
@@ -1,7 +1,7 @@
 # Configure Auth0 as an OIDC identity provider for EKS user authentication
 # This allows users to authenticate to the EKS cluster using Auth0 credentials
 resource "awscc_eks_identity_provider_config" "auth0_idp" {
-  cluster_name = awscc_eks_cluster.eks_cluster.name
+  cluster_name = var.cluster_name
   type         = "oidc"
 
   oidc = {
@@ -11,16 +11,8 @@ resource "awscc_eks_identity_provider_config" "auth0_idp" {
     username_claim = "email"
     groups_prefix  = var.oicd_groups_prefix # Like: "auth0:eks-cluster" 
   }
+}
 
-  tags = [
-    {
-      key   = "Provider"
-      value = "Auth0"
-    },
-    {
-      key   = "Modified By"
-      value = "AWSCC"
-    }
-  ]
-
+variable "cluster_name" {
+  type = string
 }
