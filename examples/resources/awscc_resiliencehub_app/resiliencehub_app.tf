e06d5d1a0eb31ce02a282c8f3639ab75a7c53cc5
LearningNewbie <35469293+LearningNewbie@users.noreply.github.com>
2024-11-18 11:03:45 -0500

    Update resiliencehub_app.tf

--- examples/resources/awscc_resiliencehub_app/resiliencehub_app.tf
+++ examples/resources/awscc_resiliencehub_app/resiliencehub_app.tf
@@ -1,34 +1,43 @@
-resource "awscc_resiliencehub_app" "main" {
-  name             = "test-app"
-  resource_mappings = [ {
-      physical_resource_id = {
-        identifier = "s3://terraform-state-files/test-app.tfstate"
-        type = "Native"
-        # Optional aws_region = ""
-        # Optional aws_account_id = ""
-      }
-      mapping_type = "Terraform"
-      # Optional  resource_name = ""
-      #           resource_type = ""
-      #           logical_resource = { 
-      #             logical_resource_name = ""
-      #             resource_mapping_name = ""
-      #             resource_mapping_arn = awscc_resiliencehub_app.app.arn 
-      }]
-  resiliency_policy_arn = "arn:aws:resiliencehub:us-west-1:<account-id>:resiliency-policy/<id>"
-  app_template_body = <<EOF
-    { "resources": [], "appComponents": [], "excludedResources": { "logicalResourceIds": [] }, "version": 2 }
-EOF
+resource "awscc_resiliencehub_app" "example" {
+  name        = "test-app"
   description = "This is a test app."
+  app_template_body = jsonencode({
+    format_version = 2
+    resources = [{
+      name = ""
+      type = ""
+      parameters = {
+        tags = {
+          env = "dev"
+        }
+      }
+    }]
+  })
+  resiliency_policy_arn = awscc_resiliencehub_resiliency_policy.example.arn
+  resource_mappings = [{
+    physical_resource_id = {
+      identifier     = "s3://terraform-state-files/test-app.tfstate"
+      type           = "Native"
+      aws_region     = ""
+      aws_account_id = ""
+    }
+    mapping_type  = "Terraform"
+    resource_name = ""
+    resource_type = ""
+    logical_resource = {
+      logical_resource_name = ""
+      resource_mapping_name = ""
+      resource_mapping_arn  = awscc_resiliencehub_app.example.arn
+    }
+  }]
   app_assessment_schedule = "Daily"
-  event_subscriptions = []
+  event_subscriptions     = []
   permission_model = {
-    type = "RoleBased"
-    # Optional invoker_role_name = "resilience-hub-test-app-role"
+    type              = "RoleBased"
+    invoker_role_name = "resilience-hub-test-app-role"
   }
   tags = [{
     key   = "Modified By"
     value = "AWSCC"
   }]
 }
-
