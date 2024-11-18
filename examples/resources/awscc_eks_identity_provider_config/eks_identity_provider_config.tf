76a03bfad72d7c76ed4c57f11a99be17f550c174
LearningNewbie <35469293+LearningNewbie@users.noreply.github.com>
2024-11-18 14:11:59 -0500

    Update eks_identity_provider_config.tf

--- examples/resources/awscc_eks_identity_provider_config/eks_identity_provider_config.tf
+++ examples/resources/awscc_eks_identity_provider_config/eks_identity_provider_config.tf
@@ -2,12 +2,12 @@ resource "awscc_eks_identity_provider_config" "identity_provider_config" {
   cluster_name                  = awscc_eks_cluster.eks_cluster.name
   type                          = "oidc"
   identity_provider_config_name = "identity_provider_config"
-  
+
   oidc {
     client_id  = "sts.amazonaws.com"
-    issuer_url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+    issuer_url = "https://${awscc_iam_oidc_provider.example.url}"
   }
-  
+
   tags = [{
     key   = "Modified By"
     value = "AWSCC"
