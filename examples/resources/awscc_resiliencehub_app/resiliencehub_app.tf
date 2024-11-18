3895249e0213cfb6423afbb6d519c43080cb522d
LearningNewbie <35469293+LearningNewbie@users.noreply.github.com>
2024-11-18 13:54:16 -0500

    Update resiliencehub_app.tf

--- examples/resources/awscc_resiliencehub_app/resiliencehub_app.tf
+++ examples/resources/awscc_resiliencehub_app/resiliencehub_app.tf
@@ -1,11 +1,11 @@
-resource "awscc_resiliencehub_app" "example" {
+resource "awscc_resiliencehub_app" "main" {
   name        = "test-app"
   description = "This is a test app."
   app_template_body = jsonencode({
     format_version = 2
     resources = [{
-      name = ""
-      type = ""
+      name = "test-app"
+      type = "AWS::EC2::Instance"
       parameters = {
         tags = {
           env = "dev"
@@ -13,25 +13,28 @@ resource "awscc_resiliencehub_app" "example" {
       }
     }]
   })
-  resiliency_policy_arn = awscc_resiliencehub_resiliency_policy.example.arn
+  resiliency_policy_arn = awscc_resiliencehub_resiliency_policy.test.arn
   resource_mappings = [{
     physical_resource_id = {
       identifier     = "s3://terraform-state-files/test-app.tfstate"
       type           = "Native"
-      aws_region     = ""
-      aws_account_id = ""
+      aws_region     = "us-west-2"
+      aws_account_id = "112223333444"
     }
     mapping_type  = "Terraform"
-    resource_name = ""
-    resource_type = ""
+    resource_name = "test-app"
+    resource_type = "AWS::EC2::Instance"
     logical_resource = {
-      logical_resource_name = ""
-      resource_mapping_name = ""
-      resource_mapping_arn  = awscc_resiliencehub_app.example.arn
+      logical_resource_name = "test-app"
+      resource_mapping_arn  = awscc_resiliencehub_app.app.arn
     }
+
+  }]
+  event_subscriptions = [{
+    name          = "test-app"
+    sns_topic_arn = awscc_sns_topic.sns_example.arn
   }]
   app_assessment_schedule = "Daily"
-  event_subscriptions     = []
   permission_model = {
     type              = "RoleBased"
     invoker_role_name = "resilience-hub-test-app-role"
