21c5931643fd36616d3477b5e51ad312603c5090
LearningNewbie <35469293+LearningNewbie@users.noreply.github.com>
2024-11-18 11:34:43 -0500

    Update resiliency_policy.tf

--- examples/resources/awscc_resiliencehub_resiliency_policy/resiliency_policy.tf
+++ examples/resources/awscc_resiliencehub_resiliency_policy/resiliency_policy.tf
@@ -3,20 +3,20 @@ resource "awscc_resiliencehub_resiliency_policy" "example" {
   tier        = "MissionCritical"
   policy = {
     software = {
-      rpo_in_secs = 3600
+      rpo_in_secs = 900
       rto_in_secs = 3600
     }
     hardware = {
-      rpo_in_secs = 3600
-      rto_in_secs = 3600
+      rpo_in_secs = 300
+      rto_in_secs = 300
     }
     region = {
-      rpo_in_secs = 1200
-      rto_in_secs = 1200
+      rpo_in_secs = 300
+      rto_in_secs = 300
     }
     az = {
-      rpo_in_secs = 1200
-      rto_in_secs = 1200
+      rpo_in_secs = 300
+      rto_in_secs = 300
     }
   }
   policy_description       = "This is an example policy"
