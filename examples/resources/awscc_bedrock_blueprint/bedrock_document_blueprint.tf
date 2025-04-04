b38e5b132b9b5b260d71cfb90a280baa475ee051
lodgei <lodgei@amazon.com>
2025-04-04 16:45:02 +0000

    updated blueprints

--- examples/resources/awscc_bedrock_blueprint/bedrock_document_blueprint.tf
+++ examples/resources/awscc_bedrock_blueprint/bedrock_document_blueprint.tf
@@ -1,32 +1,23 @@
-
-resources "awscc_bedrockdataautomation_blueprint" "example" {
-    blueprint_name = "EXAMPLE"
+resource "awscc_bedrock_blueprint" "example" {
+    blueprint_name = "example-blueprint"
     type = "DOCUMENT"
-    blueprint_stage = "LIVE"
-    schema = 
-}
-
-
-bda_create_blueprint_response = bedrock_data_automation_client.create_blueprint(
-    blueprintName='CUSTOM_PAYSLIP_BLUEPRINT',
-    type='DOCUMENT',
-    blueprintStage='LIVE',
-    schema=json.dumps({
-        '$schema': 'http://json-schema.org/draft-07/schema#',
-        'description': 'default',
-        'documentClass': 'default',
-        'type': 'object',
-        'properties': {
-            'gross_pay_this_period': {
-                'type': 'number',
-                'inferenceType': 'extractive',
-                'description': 'The gross pay for this pay period from the Earnings table'
+    schema = jsonencode({
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "description": "default",
+        "class": "default",
+        "type": "object",
+        "definitions": {},
+        "properties": {
+            "gross_pay_this_period": {
+                "type": "number",
+                "inferenceType": "explicit",
+                "instruction": "The gross pay for this pay period from the Earnings table"
             },
-            'net_pay': {
-                'type': 'number',
-                'inferenceType': 'extractive',
-                'description': 'The net pay for this pay period from the bottom of the document'
+            "net_pay": {
+                "type": "number",
+                "inferenceType": "explicit",
+                "instruction": "The net pay for this pay period from the bottom of the document"
             }
         }
-    }),
-)
+    })
+}
