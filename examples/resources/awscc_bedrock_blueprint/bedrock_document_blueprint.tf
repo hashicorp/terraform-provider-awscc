4bcd1049dbf39183d64dd2660e0b89219b507b1d
lodgei <lodgei@amazon.com>
2025-04-04 16:56:21 +0000

    make docs

--- examples/resources/awscc_bedrock_blueprint/bedrock_document_blueprint.tf
+++ examples/resources/awscc_bedrock_blueprint/bedrock_document_blueprint.tf
@@ -1,23 +1,23 @@
 resource "awscc_bedrock_blueprint" "example" {
-    blueprint_name = "example-blueprint"
-    type = "DOCUMENT"
-    schema = jsonencode({
-        "$schema": "http://json-schema.org/draft-07/schema#",
-        "description": "default",
-        "class": "default",
-        "type": "object",
-        "definitions": {},
-        "properties": {
-            "gross_pay_this_period": {
-                "type": "number",
-                "inferenceType": "explicit",
-                "instruction": "The gross pay for this pay period from the Earnings table"
-            },
-            "net_pay": {
-                "type": "number",
-                "inferenceType": "explicit",
-                "instruction": "The net pay for this pay period from the bottom of the document"
-            }
-        }
-    })
+  blueprint_name = "example-blueprint"
+  type           = "DOCUMENT"
+  schema = jsonencode({
+    "$schema" : "http://json-schema.org/draft-07/schema#",
+    "description" : "default",
+    "class" : "default",
+    "type" : "object",
+    "definitions" : {},
+    "properties" : {
+      "gross_pay_this_period" : {
+        "type" : "number",
+        "inferenceType" : "explicit",
+        "instruction" : "The gross pay for this pay period from the Earnings table"
+      },
+      "net_pay" : {
+        "type" : "number",
+        "inferenceType" : "explicit",
+        "instruction" : "The net pay for this pay period from the bottom of the document"
+      }
+    }
+  })
 }
