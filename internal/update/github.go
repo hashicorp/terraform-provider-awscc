aa18a4a528ede87ac675c58100cea15a574c2f4e
ThomasZalewski <thomas.zalewski@hashicorp.com>
2025-07-25 15:14:19 -0700

    Update github.go

--- internal/update/github.go
+++ internal/update/github.go
@@ -177,6 +177,7 @@ func createPullRequest(ctx context.Context, config *GitHubConfig, changes *[]str
 }
 
 func createIssue(ctx context.Context, resource string, error string, config *GitHubConfig, repositoryLink string) (string, error) {
+	return "", nil
 	// Return empty values when running in test environment
 	if isRunningInTest() {
 		return "", nil
@@ -405,7 +406,7 @@ func createFormattedIssue(description, affectedResources string) string {
 * Please vote on this issue by adding a 👍 [reaction](https://blog.github.com/2016-03-10-add-reactions-to-pull-requests-issues-and-comments/) to the original issue to help the community and maintainers prioritize this request
 * Please do not leave "+1" or other comments that do not add relevant new information or questions, they generate extra noise for issue followers and do not help prioritize the request
 * If you are interested in working on this issue or have submitted a pull request, please leave a comment
-* The resources and data sources in this provider are generated from the CloudFormation schema, so they can only support the actions that the underlying schema supports. For this reason submitted bugs should be limited to defects in the generation and runtime code of the provider. Customizing behavior of the resource, or noting a gap in behavior are not valid bugs and should be submitted as enhancements to AWS via the CloudFormation Open Coverage Roadmap.
+* The resources and data sources in this providerf are generated from the CloudFormation schema, so they can only support the actions that the underlying schema supports. For this reason submitted bugs should be limited to defects in the generation and runtime code of the provider. Customizing behavior of the resource, or noting a gap in behavior are not valid bugs and should be submitted as enhancements to AWS via the CloudFormation Open Coverage Roadmap.
 
 <!--- Thank you for keeping this note for the community --->
 
