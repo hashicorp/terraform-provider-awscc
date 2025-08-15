076eabb5ef6880b0db3f3893d6a8514a4a9ebdee
Adrian Johnson <adrian.johnson@hashicorp.com>
2025-08-15 17:53:41 -0400

    organization

--- internal/identity/names/services.go
+++ internal/identity/names/services.go
@@ -1,12 +1,30 @@
-package main
+package names
 
-//type Service struct {
-//	IsGlobal    bool       `hcl:"is_global,optional"`
-//	ServiceName string     `hcl:",label"`
-//	Resources   []Resource `hcl:"resource,block"`
-//}
-//
-//type Resource struct {
-//	TFResourceName     string `hcl:"tf_resource_name"`
-//	HasMutableIdentity bool   `hcl:"has_mutable_identity,optional"`
-//}
+import (
+	"github.com/hashicorp/hcl/v2/hclsimple"
+)
+
+type Config struct {
+	Services []Service `hcl:"service,block"`
+}
+
+type Service struct {
+	IsGlobal    bool       `hcl:"is_global,optional"`
+	ServiceName string     `hcl:",label"`
+	Resources   []Resource `hcl:"resource,block"`
+}
+
+type Resource struct {
+	TFResourceName     string `hcl:"tf_resource_name"`
+	HasMutableIdentity bool   `hcl:"has_mutable_identity,optional"`
+}
+
+func ParseServicesFile() (*Config, error) {
+	var config Config
+	err := hclsimple.DecodeFile("services.hcl", nil, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
