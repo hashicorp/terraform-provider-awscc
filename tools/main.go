d8a420f5bffffccc38ef9ad7ce2d554a5a1dec3b
Dirk Avery <dirk.avery@gmail.com>
2025-12-18 16:45:16 -0500

    Update golangci-lint to v2

--- tools/main.go
+++ tools/main.go
@@ -9,7 +9,7 @@ package main
 import (
 	_ "github.com/YakDriver/copyplop"
 	_ "github.com/client9/misspell/cmd/misspell"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 	_ "github.com/pavius/impi/cmd/impi"
 )
