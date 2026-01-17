1c88b75aa6851fc52f952d7f78ce3ad13722eb6c
Manu Chandrasekhar <manuchn@amazon.com>
2026-01-17 15:08:50 -0500

    docs: added examples for connet data table and attribute

---
+++ examples/resources/awscc_connect_data_table/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
+
+provider "awscc" {
+  region = "us-east-1"
+}
+
+provider "random" {}
+
+# Create Connect instance first
+resource "random_id" "instance" {
+  byte_length = 4
+}
+
+resource "awscc_connect_instance" "example" {
+  identity_management_type = "CONNECT_MANAGED"
+  instance_alias           = "example-connect-instance-${random_id.instance.hex}"
+
+  attributes = {
+    inbound_calls  = true
+    outbound_calls = true
+  }
+
+  tags = [{
+    key   = "Environment"
+    value = "test"
+  }]
+}
+
+# Create Connect data table with required properties
+resource "awscc_connect_data_table" "example" {
+  instance_arn     = awscc_connect_instance.example.arn
+  name             = "example-data-table"
+  description      = "Example data table for testing"
+  status           = "PUBLISHED"
+  time_zone        = "UTC"
+  value_lock_level = "NONE"
+
+  tags = [{
+    key   = "Environment"
+    value = "test"
+  }]
+}
