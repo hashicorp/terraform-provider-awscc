070d867e7d2d02e669e8d40a3c0ef20e81267f0f
Chris Geiger <cgei@amazon.com>
2026-02-05 16:53:06 -0800

    remove bin file and re-add test

---
+++ test/main.tf
@@ -0,0 +1,66 @@
+# Copyright IBM Corp. 2021, 2026
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    awscc = {
+      source = "hashicorp/awscc"
+    }
+  }
+}
+
+provider "aws" {}
+provider "awscc" {}
+
+resource "awscc_kms_key" "test" {
+  key_policy = jsonencode({
+    Id = "kms-tf-1"
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+# resource "awscc_ecs_task_definition" "test" {
+#   container_definitions = [
+#     {
+#       command = ["sleep", "10"]
+#       cpu     = 10
+#       docker_labels = {
+#         "env" = "testing"
+#       }
+#       entry_point = ["/"]
+#       environment = [
+#         {
+#           name  = "VARNAME"
+#           value = "VARVAL"
+#         }
+#       ]
+#       essential = true
+#       image     = "jenkins/jenkins:lts"
+#       memory    = 128
+#       name      = "jenkins"
+#       port_mappings = [
+#         {
+#           container_port = 80
+#           host_port      = 8080
+#         }
+#       ]
+#     }
+#   ]
+# }
