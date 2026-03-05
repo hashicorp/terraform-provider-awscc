0d060490d1b73a42831aaa3376ed6def8195debc
Manu Chandrasekhar <manuchn@amazon.com>
2026-03-05 08:22:59 -0500

    fix: policy example

--- examples/resources/awscc_bedrockagentcore_policy/bedrockagentcore_policy.tf
+++ examples/resources/awscc_bedrockagentcore_policy/bedrockagentcore_policy.tf
@@ -2,21 +2,6 @@ resource "awscc_bedrockagentcore_policy_engine" "example" {
   name        = "ExamplePolicyEngine"
   description = "Example policy engine for BedrockAgentCore"
 
-  policy_statements = {
-    statements = [
-      {
-        statement = <<-EOT
-          permit(
-            principal,
-            action,
-            resource is AgentCore::Gateway
-          );
-        EOT
-      }
-    ]
-  }
-
-  validation_mode = "IGNORE_ALL_FINDINGS"
 }
 
 resource "awscc_bedrockagentcore_policy" "example" {
