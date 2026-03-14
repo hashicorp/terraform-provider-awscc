b91bf56985e663ac2a4ecf276b1dbc4cc947409e
Manu Chandrasekhar <manuchn@amazon.com>
2026-03-14 12:02:07 -0400

    fix: removed unused blocks, formatting

--- examples/resources/awscc_cleanroomsml_configured_model_algorithm_association/cleanroomsml_configured_model_algorithm_association.tf
+++ examples/resources/awscc_cleanroomsml_configured_model_algorithm_association/cleanroomsml_configured_model_algorithm_association.tf
@@ -1,22 +1,8 @@
-
-terraform {
-  required_providers {
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = "~> 1.75.0"
-    }
-  }
-}
-
-provider "awscc" {
-  region = "us-east-1"
-}
-
 resource "awscc_cleanroomsml_configured_model_algorithm_association" "example" {
   configured_model_algorithm_arn = "arn:aws:cleanroomsml:us-east-1:123456789012:configured-model-algorithm/example-algorithm"
   membership_identifier          = "example-membership-id"
-  name                          = "example-configured-model-algorithm-association"
-  
+  name                           = "example-configured-model-algorithm-association"
+
   tags = {
     Environment = "test"
     Project     = "example"
