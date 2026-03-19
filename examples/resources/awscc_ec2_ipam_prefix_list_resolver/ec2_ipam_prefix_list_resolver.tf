84cc64a8942ded824ad9be9e7175ffb7923308dc
quixoticmonk <manuchn@amazon.com>
2026-03-19 16:10:28 -0400

    Add example for awscc_ec2_ipam_prefix_list_resolver
    This commit adds a validated Terraform example for the awscc_ec2_ipam_prefix_list_resolver resource.
    
    Files added:
    - examples/resources/awscc_ec2_ipam_prefix_list_resolver/ec2_ipam_prefix_list_resolver.tf
    - templates/resources/awscc_ec2_ipam_prefix_list_resolver.md.tmpl
    - docs/resources/awscc_ec2_ipam_prefix_list_resolver.md (generated by make docs-all)
    
    The example has been validated through real AWS deployment testing
    and passes all HashiCorp validation requirements.
    
    Generated by TANGO Multi-Agent Pipeline

---
+++ examples/resources/awscc_ec2_ipam_prefix_list_resolver/ec2_ipam_prefix_list_resolver.tf
@@ -0,0 +1,52 @@
+resource "awscc_ec2_ipam" "example" {
+  description = "Example IPAM for prefix list resolver"
+
+  operating_regions = [
+    {
+      region_name = "us-east-1"
+    },
+    {
+      region_name = "us-west-2"
+    }
+  ]
+
+  tags = [{
+    key   = "Name"
+    value = "example-ipam"
+  }, {
+    key   = "Environment"
+    value = "example"
+  }]
+}
+
+resource "awscc_ec2_ipam_scope" "example" {
+  ipam_id     = awscc_ec2_ipam.example.id
+  description = "Example IPAM scope"
+
+  tags = [{
+    key   = "Name"
+    value = "example-ipam-scope"
+  }, {
+    key   = "Environment"
+    value = "example"
+  }]
+}
+
+resource "awscc_ec2_ipam_prefix_list_resolver" "example" {
+  ipam_id        = awscc_ec2_ipam.example.id
+  address_family = "IPv4"
+  description    = "Example IPAM prefix list resolver"
+
+  rules = [{
+    rule_type   = "static-cidr"
+    static_cidr = "10.0.0.0/16"
+  }]
+
+  tags = [{
+    key   = "Name"
+    value = "example-prefix-list-resolver"
+  }, {
+    key   = "Environment"
+    value = "example"
+  }]
+}
